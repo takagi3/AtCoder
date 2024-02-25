4e7b0beac5ed3a4eaa26ee29427693619ccb95d7
takagi3 <satoshi.takagi@pm.me>
2024-02-26 00:36:05 +0900

    2024/02/26

--- math-and-algorithm/src/bin/011.rs
+++ math-and-algorithm/src/bin/011.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u64,
+    }
+
+    let mut ans: Vec<u64> = Vec::new();
+    for i in 2..=n {
+        let mut is_prime: bool = true;
+        for j in 2..i {
+            if i % j == 0 {
+                is_prime = false;
+                break;
+            }
+        }
+        if is_prime {
+            ans.push(i);
+        }
+    }
+
+    println!("{}", ans
+        .iter()
+        .map(std::string::ToString::to_string)
+        .collect::<Vec<_>>()
+        .join(" "));
 }
--- math-and-algorithm/src/bin/012.rs
+++ math-and-algorithm/src/bin/012.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u64,
+    }
+
+    for i in 2..=(n as f64).sqrt() as u64 {
+        if n % i == 0 {
+            println!("No");
+            return;
+        }
+    }
+
+    println!("Yes");
 }
