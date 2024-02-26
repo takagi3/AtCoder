02befde28452acdcd27123e887cd871f8266ac86
takagi3 <satoshi.takagi@pm.me>
2024-02-27 00:23:13 +0900

    2024/02/27

--- math-and-algorithm/src/bin/013.rs
+++ math-and-algorithm/src/bin/013.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u64,
+    }
+
+    let mut ans: Vec<u64> = Vec::new();
+    for i in 1..=(n as f64).sqrt() as u64 {
+        if n % i == 0 {
+            ans.push(i);
+            if i * i != n {
+                ans.push(n / i);
+            }
+        }
+    }
+
+    println!("{}", ans
+        .iter()
+        .map(std::string::ToString::to_string)
+        .collect::<Vec<_>>()
+        .join("\n")
+    );
 }
--- math-and-algorithm/src/bin/014.rs
+++ math-and-algorithm/src/bin/014.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u64,
+    }
+
+    let mut ans: Vec<u64> = Vec::new();
+    let mut rem: u64 = n;
+
+    loop {
+        let mut is_end: bool = true;
+        for i in 2..=(n as f64).sqrt() as u64 {
+            if rem % i == 0 {
+                ans.push(i);
+                rem /= i;
+                is_end = false;
+                break;
+            }
+        }
+        if is_end { break; }
+    }
+
+    if rem > 1 { ans.push(rem); }
+
+    println!("{}", ans
+        .iter()
+        .map(std::string::ToString::to_string)
+        .collect::<Vec<_>>()
+        .join(" ")
+    );
 }
