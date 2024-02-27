95768e35e027f8cb298ce5985c07d824844b040d
takagi3 <satoshi.takagi@pm.me>
2024-02-28 01:41:12 +0900

    2024/02/28

--- math-and-algorithm/src/bin/015.rs
+++ math-and-algorithm/src/bin/015.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+
+fn gcd(a: u64, b: u64) -> u64 {
+    return if a % b == 0 { b } else { gcd(b, a % b) }
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: u64,
+        b: u64,
+    }
+
+    println!("{}", gcd(a, b));
 }
--- math-and-algorithm/src/bin/016.rs
+++ math-and-algorithm/src/bin/016.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
+fn gcd(a: u64, b: u64) -> u64 {
+    return if a % b == 0 { b } else { gcd(b, a % b) }
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [u64; n],
+    }
+
+    let mut ans: u64 = a[0];
+    for i in 1..n {
+        ans = gcd(a[i], ans);
+    }
+
+    println!("{}", ans);
 }
