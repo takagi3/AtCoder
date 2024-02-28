f826a2a0da8044bfc97dfaa4849a9855ad8641af
takagi3 <satoshi.takagi@pm.me>
2024-02-29 01:19:26 +0900

    2024/02/29

--- math-and-algorithm/src/bin/017.rs
+++ math-and-algorithm/src/bin/017.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
+fn gcd(a: u64, b: u64) -> u64 {
+    return if a % b == 0 { b } else { gcd(b, a % b) };
+}
+
+fn lcd(a: u64, b: u64) -> u64 {
+    return a / gcd(a, b) * b;
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [u64; n],
+    }
+
+    let mut ans: u64 = a[0];
+    for i in 1..n {
+        ans = lcd(a[i], ans);
+    }
+
+    println!("{}", ans);
 }
--- math-and-algorithm/src/bin/018.rs
+++ math-and-algorithm/src/bin/018.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut cnt: Vec<u64> = vec![0; 5];
+    for i in 0..n {
+        cnt[a[i] / 100] += 1;
+    }
+
+    println!("{}", cnt[1] * cnt[4] + cnt[2] * cnt[3]);
 }
