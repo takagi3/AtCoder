0dc4d768e7df3d0bbb0a60ec676bc81de2e79c8d
takagi3 <satoshi.takagi@pm.me>
2024-03-01 00:57:06 +0900

    2024/03/01

--- math-and-algorithm/src/bin/019.rs
+++ math-and-algorithm/src/bin/019.rs
@@ -1,3 +1,31 @@
+use proconio::input;
+
+fn fact(n: u64) -> u64 {
+    let mut ret: u64 = 1;
+    for i in 1..=n { ret *= i; }
+    return ret;
+}
+
+fn perm(n: u64, r: u64) -> u64 {
+    let mut ret: u64 = 1;
+    for i in 0..r { ret *= n - i; }
+    return ret;
+}
+
+fn comb(n: u64, r: u64) -> u64 {
+    return perm(n, r) / fact(r);
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut cnt: Vec<u64> = vec![0; 4];
+    for i in 0..n {
+        cnt[a[i]] += 1;
+    }
+
+    println!("{}", comb(cnt[1], 2) + comb(cnt[2], 2) + comb(cnt[3], 2));
 }
--- math-and-algorithm/src/bin/020.rs
+++ math-and-algorithm/src/bin/020.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [u64; n],
+    }
+
+    let mut ans: u64 = 0;
+    for i in 0..n - 4 {
+        for j in i + 1..n - 3 {
+            for k in j + 1..n - 2 {
+                for l in k + 1..n - 1 {
+                    for m in l + 1..n {
+                        if a[i] + a[j] + a[k] + a[l] + a[m] == 1000 {
+                            ans += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
