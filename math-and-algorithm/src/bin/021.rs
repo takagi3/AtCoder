1245e43e3f295eea7a9054659154899e1b1d28bf
takagi3 <satoshi.takagi@pm.me>
2024-03-02 03:32:18 +0900

    2024/03/02

--- math-and-algorithm/src/bin/021.rs
+++ math-and-algorithm/src/bin/021.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
+fn fact(n: u64) -> u64 {
+    let mut ret: u64 = 1;
+    for i in 1..=n { ret *= i; }
+    return ret;
+}
+
+fn perm(n: u64, r: u64) -> u64 {
+    let mut ret: u64 = 1;
+    for i in 0..r { ret *= n - i; }
+    return ret;
+}
+
+fn comb(n: u64, r: u64) -> u64 {
+    return perm(n, r) / fact(r);
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u64,
+        r: u64,
+    }
+
+    println!("{}", comb(n, r));
 }
