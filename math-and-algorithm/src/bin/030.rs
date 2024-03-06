6788b1203d924d0d473065c90c51f20f3e78735c
takagi3 <satoshi.takagi@pm.me>
2024-03-07 01:56:14 +0900

    2024/03/07

--- math-and-algorithm/src/bin/030.rs
+++ math-and-algorithm/src/bin/030.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+use std::cmp::max;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        w: usize,
+        wv: [(usize, i64); n],
+    }
+
+    let mut dp: Vec<Vec<i64>> = vec![vec![i64::MIN; w + 1]; n + 1];
+    dp[0][0] = 0;
+    for i in 1..=n {
+        for j in 0..=w {
+            if j >= wv[i - 1].0 {
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - wv[i - 1].0] + wv[i - 1].1)
+            } else {
+                dp[i][j] = dp[i - 1][j]
+            }
+        }
+    }
+
+    println!("{}", dp[n].iter().max().unwrap());
 }
