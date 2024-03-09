88a6ff0f454b91b4d8ebd42e8b9b9d93d375d836
takagi3 <satoshi.takagi@pm.me>
2024-03-09 15:59:15 +0900

    2024/03/09

--- math-and-algorithm/src/bin/031.rs
+++ math-and-algorithm/src/bin/031.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+use std::cmp::max;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        a: [u64; n],
+    }
+
+    let mut dp: Vec<u64> = vec![0; n + 1];
+    dp[1] = a[0];
+    dp[2] = a[1];
+    for i in 3..=n {
+        dp[i] = max(dp[i - 2], dp[i - 3]) + a[i - 1]
+    }
+
+    println!("{}", max(dp[n], dp[n - 1]));
 }
