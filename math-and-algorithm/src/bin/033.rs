7f65003f97d72b444355eba77ad3ea2200af00d0
takagi3 <satoshi.takagi@pm.me>
2024-03-11 02:55:56 +0900

    2024/03/11

--- math-and-algorithm/src/bin/033.rs
+++ math-and-algorithm/src/bin/033.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        ax: i64,
+        ay: i64,
+        bx: i64,
+        by: i64,
+        cx: i64,
+        cy: i64,
+    }
+
+    let ans: f64;
+    if (ax - bx) * (cx - bx) + (ay - by) * (cy - by) <= 0 {
+        ans = (((ax - bx).pow(2) + (ay - by).pow(2)) as f64).sqrt();
+    } else if (ax - cx) * (bx - cx) + (ay - cy) * (by - cy) <= 0 {
+        ans = (((ax - cx).pow(2) + (ay - cy).pow(2)) as f64).sqrt();
+    } else {
+        let area: f64 = (((ax - bx) * (cy - by) - (ay - by) * (cx - bx)) as f64).abs();
+        let bc: f64 = (((bx - cx).pow(2) + (by - cy).pow(2)) as f64).sqrt();
+        ans = area / bc;
+    }
+
+    println!("{}", ans);
 }
