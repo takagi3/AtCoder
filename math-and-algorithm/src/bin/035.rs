44a873cfcb0a243c65b091a4e86d4ec45f51a573
takagi3 <satoshi.takagi@pm.me>
2024-03-13 02:23:30 +0900

    2024/03/13

--- math-and-algorithm/src/bin/035.rs
+++ math-and-algorithm/src/bin/035.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        x1: f64,
+        y1: f64,
+        r1: f64,
+        x2: f64,
+        y2: f64,
+        r2: f64,
+    }
+
+    let dist_xy: f64 = ((x2 - x1).powf(2f64) + (y2 - y1).powf(2f64)).sqrt();
+    let ans: u32;
+    if r1.max(r2) > dist_xy + r1.min(r2) {
+        ans = 1;
+    } else if r1.max(r2) == dist_xy + r1.min(r2) {
+        ans = 2;
+    } else if dist_xy == r1 + r2 {
+        ans = 4;
+    } else if dist_xy > r1 + r2 {
+        ans = 5;
+    } else {
+        ans = 3;
+    }
+
+    println!("{}", ans);
 }
