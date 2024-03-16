3d9fc87622582d59238002abb420b4c08e973cff
takagi3 <satoshi.takagi@pm.me>
2024-03-17 03:17:33 +0900

    2024/03/17

--- math-and-algorithm/src/bin/039.rs
+++ math-and-algorithm/src/bin/039.rs
@@ -1,3 +1,34 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        q: usize,
+        lrx: [(usize, usize, i32); q],
+    }
+
+    let mut b: Vec<i32> = vec![0; n + 1];
+    for i in 0..q {
+        b[lrx[i].0] += lrx[i].2;
+        if lrx[i].1 < n {
+            b[lrx[i].1 + 1] -= lrx[i].2;
+        }
+    }
+
+    let mut ans: Vec<&str> = vec![""; n - 1];
+    for i in 0..n - 1 {
+        if b[i + 2] > 0 {
+            ans[i] = "<";
+        } else if b[i + 2] < 0 {
+            ans[i] = ">";
+        } else {
+            ans[i] = "=";
+        }
+    }
+
+    println!("{}", ans.iter()
+        .map(std::string::ToString::to_string)
+        .collect::<Vec<_>>()
+        .join("")
+    );
 }
