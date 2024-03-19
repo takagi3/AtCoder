cfe6161dd352c57d766410e542197a0e8ea7bf8a
takagi3 <satoshi.takagi@pm.me>
2024-03-20 02:47:25 +0900

    2024/03/20

--- math-and-algorithm/src/bin/042.rs
+++ math-and-algorithm/src/bin/042.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+    }
+
+    let mut div: Vec<usize> = vec![0; n + 1];
+    for i in 1..=n {
+        for j in (i..=n).step_by(i) {
+            div[j] += 1
+        }
+    }
+
+    let mut ans: usize = 0;
+    for i in 1..=n {
+        ans += div[i] * i
+    }
+
+    println!("{}", ans);
 }
