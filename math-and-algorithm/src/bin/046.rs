b79a56b9d2a0263ce784afe45bf809287a04b068
takagi3 <satoshi.takagi@pm.me>
2024-03-27 02:02:47 +0900

    2024/03/27

--- math-and-algorithm/src/bin/046.rs
+++ math-and-algorithm/src/bin/046.rs
@@ -1,18 +1,51 @@
 use proconio::input;
 use proconio::marker::Chars;
+use std::collections::VecDeque;
 
 fn main() {
     input! {
         r: usize,
         c: usize,
-        sy: u32,
-        sx: u32,
-        gy: u32,
-        gx: u32,
+        start: (usize, usize),
+        goal: (usize, usize),
         map: [Chars; r],
     }
 
-    let ans: u64 = 0;
+    let mut g: Vec<Vec<Vec<(usize, usize)>>> = vec![vec![vec![]; c]; r];
 
-    println!("{}", ans);
+    for i in 1..r - 1 {
+        for j in 1..c - 1 {
+            if map[i][j] == '.' {
+                if map[i - 1][j] == '.' {
+                    g[i][j].push((i - 1, j))
+                }
+                if map[i + 1][j] == '.' {
+                    g[i][j].push((i + 1, j))
+                }
+                if map[i][j - 1] == '.' {
+                    g[i][j].push((i, j - 1))
+                }
+                if map[i][j + 1] == '.' {
+                    g[i][j].push((i, j + 1))
+                }
+            }
+        }
+    }
+
+    let mut q: VecDeque<(usize, usize)> = VecDeque::new();
+    q.push_back((start.0 - 1, start.1 - 1));
+    let mut dist: Vec<Vec<u32>> = vec![vec![u32::MAX; c]; r];
+    dist[start.0 - 1][start.1 - 1] = 0;
+
+    while !q.is_empty() {
+        let p = q.pop_front().unwrap();
+        for nex in &g[p.0][p.1] {
+            if dist[nex.0][nex.1] > dist[p.0][p.1] + 1 {
+                dist[nex.0][nex.1] = dist[p.0][p.1] + 1;
+                q.push_back((nex.0, nex.1));
+            }
+        }
+    }
+
+    println!("{}", dist[goal.0 - 1][goal.1 - 1]);
 }
