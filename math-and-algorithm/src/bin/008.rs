6e8859914455981789fdb52bfa088dce54fb309b
takagi3 <satoshi.takagi@pm.me>
2024-04-02 00:34:39 +0900

    2024/04/02

--- math-and-algorithm/src/bin/008.rs
+++ math-and-algorithm/src/bin/008.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: u32,
+        s: u32,
+    }
+
+    let mut ans: u32 = 0;
+    for i in 1..=n {
+        for j in 1..=n {
+            if i + j <= s {
+                ans += 1
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
--- math-and-algorithm/src/bin/009.rs
+++ math-and-algorithm/src/bin/009.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: u32,
+        a: [u32; n],
+    }
+
+    let mut ans: &str = "No";
+    for i in 0u64..(1 << n) {
+        let mut tmp_sum: u32 = 0;
+        for j in 0..n {
+            if i & (1 << j) != 0 {
+                tmp_sum += a[j]
+            }
+        }
+        if tmp_sum == s {
+            ans = "Yes";
+            break;
+        }
+    }
+
+    println!("{}", ans);
 }
