e56033bce5b95baf7662a02ed68e4b20e90390de
takagi3 <satoshi.takagi@pm.me>
2024-04-08 00:15:02 +0900

    2024/04/08

--- math-and-algorithm/src/bin/019.rs
+++ math-and-algorithm/src/bin/019.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut cnt: Vec<i64> = vec![0; 4];
+    for i in 0..n {
+        cnt[a[i]] += 1
+    }
+    let ans: i64 = ((cnt[1] * (cnt[1] - 1)) + (cnt[2] * (cnt[2] - 1)) + (cnt[3] * (cnt[3] - 1))) / 2;
+
+    println!("{}", ans);
 }
--- math-and-algorithm/src/bin/020.rs
+++ math-and-algorithm/src/bin/020.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [u32; n],
+    }
+
+    let mut ans: u32 = 0;
+    for i in 0..n - 4 {
+        for j in i + 1..n - 3 {
+            for k in j + 1..n - 2 {
+                for l in k + 1..n - 1 {
+                    for m in l + 1..n {
+                        let sum: u32 = a[i] + a[j] + a[k] + a[l] + a[m];
+                        if sum == 1000 {
+                            ans += 1
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
