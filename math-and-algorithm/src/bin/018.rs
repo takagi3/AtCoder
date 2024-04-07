4c3e76f4cdada49c3e2d674314f055ab1949ed1d
takagi3 <satoshi.takagi@pm.me>
2024-04-07 17:46:00 +0900

    2024/04/07

--- math-and-algorithm/src/bin/018.rs
+++ math-and-algorithm/src/bin/018.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut price: Vec<u32> = vec![0; 5];
+    for i in 0..n {
+        price[a[i] / 100] += 1
+    }
+    let ans: u32 = price[1] * price[4] + price[2] * price[3];
+
+    println!("{}", ans);
 }
