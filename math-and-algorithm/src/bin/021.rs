6296645936b18e1c1580d5c31d43a89e57c33c4e
takagi3 <satoshi.takagi@pm.me>
2024-04-09 01:51:26 +0900

    2024/04/09

--- math-and-algorithm/src/bin/021.rs
+++ math-and-algorithm/src/bin/021.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
+fn factorial(n: u64) -> u64 {
+    let mut ans: u64 = 1;
+    for i in 1..=n {
+        ans *= i;
+    }
+    return ans;
+}
+
+fn perm(n: u64, r: u64) -> u64 {
+    let mut ans: u64 = 1;
+    for i in 0..r {
+        ans *= n - i;
+    }
+    return ans;
+}
+
+fn comb(n: u64, r: u64) -> u64 {
+    return perm(n, r) / factorial(r);
+}
+
 fn main() {
-    todo!();
+    input! {
+        n: u64,
+        r: u64,
+    }
+
+    let ans: u64 = comb(n, r);
+
+    println!("{}", ans);
 }
--- math-and-algorithm/src/bin/022.rs
+++ math-and-algorithm/src/bin/022.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let mut cnt: Vec<u64> = vec![0; 100000];
+    for i in 0..n {
+        cnt[a[i]] += 1;
+    }
+
+    let mut ans: u64 = 0;
+    for i in 1..50000 {
+        ans += cnt[i] * cnt[100000 - i];
+    }
+    if cnt[50000] > 0 {
+        ans += cnt[50000] * (cnt[50000] - 1) / 2;
+    }
+
+    println!("{}", ans);
 }
