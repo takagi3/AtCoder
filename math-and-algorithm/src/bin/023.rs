c111a5635bd5f59504d91d417e702ff4d3f31fbc
takagi3 <satoshi.takagi@pm.me>
2024-04-10 00:36:32 +0900

    2024/04/10

--- math-and-algorithm/src/bin/023.rs
+++ math-and-algorithm/src/bin/023.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        b: [f64; n],
+        r: [f64; n],
+    }
+
+    let mut ans: f64 = 0.0;
+    for i in 0..n {
+        ans += b[i] + r[i];
+    }
+    ans /= n as f64;
+
+    println!("{}", ans);
 }
--- math-and-algorithm/src/bin/024.rs
+++ math-and-algorithm/src/bin/024.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        pq: [(f64, f64); n],
+    }
+
+    let mut ans: f64 = 0.0;
+    for i in 0..n {
+        ans += pq[i].1 / pq[i].0;
+    }
+
+    println!("{}", ans);
 }
