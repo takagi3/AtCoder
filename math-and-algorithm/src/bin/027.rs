0a535501dd136f1cca242d7f89cb905f9dbb9fed
takagi3 <satoshi.takagi@pm.me>
2024-04-12 02:54:03 +0900

    2024/04/12

--- math-and-algorithm/src/bin/027.rs
+++ math-and-algorithm/src/bin/027.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut a: [u32; n],
+    }
+
+    a.sort();
+
+    println!("{}", a.iter()
+        .map(std::string::ToString::to_string)
+        .collect::<Vec<_>>()
+        .join(" ")
+    );
 }
