11d784b431a06ff140cc3ff01c99beb99e07bab9
takagi3 <satoshi.takagi@pm.me>
2024-04-16 00:16:04 +0900

    2024/04/16

--- math-and-algorithm/src/bin/009.rs
+++ math-and-algorithm/src/bin/009.rs
@@ -3,19 +3,35 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        s: u32,
-        a: [u32; n],
+        s: usize,
+        a: [usize; n],
     }
 
     let mut ans: &str = "No";
-    for i in 0u64..(1 << n) {
-        let mut tmp_sum: u32 = 0;
-        for j in 0..n {
-            if i & (1 << j) != 0 {
-                tmp_sum += a[j]
+    // for i in 0u64..(1 << n) {
+    //     let mut tmp_sum: usize = 0;
+    //     for j in 0..n {
+    //         if i & (1 << j) != 0 {
+    //             tmp_sum += a[j]
+    //         }
+    //     }
+    //     if tmp_sum == s {
+    //         ans = "Yes";
+    //         break;
+    //     }
+    // }
+
+    let mut dp: Vec<Vec<bool>> = vec![vec![false; s + 1]; n + 1];
+    dp[0][0] = true;
+    for i in 0..n {
+        for j in 0..=s {
+            dp[i + 1][j] = if j >= a[i] && dp[i][j - a[i]] {
+                true
+            } else {
+                dp[i][j]
             }
         }
-        if tmp_sum == s {
+        if dp[i + 1][s] {
             ans = "Yes";
             break;
         }
