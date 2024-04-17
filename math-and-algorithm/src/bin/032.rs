78ae3f7770a3c32e5aa0748dfbe8a8831911ae30
takagi3 <satoshi.takagi@pm.me>
2024-04-18 00:53:38 +0900

    2024/04/18

--- math-and-algorithm/src/bin/032.rs
+++ math-and-algorithm/src/bin/032.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        x: usize,
+        mut a: [usize; n],
+    }
+
+    a.sort();
+    let mut ans: &str = "No";
+    let mut left: usize = 0;
+    let mut right: usize = n - 1;
+    let mut mid: usize;
+    while left <= right {
+        mid = (left + right) / 2;
+        if a[mid] == x {
+            ans = "Yes";
+            break;
+        } else if a[mid] < x {
+            left = mid + 1;
+        } else if a[mid] > x {
+            if mid == 0 { break; }
+            right = mid - 1;
+        }
+    }
+
+    println!("{}", ans);
 }
