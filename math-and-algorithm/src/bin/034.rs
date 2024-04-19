e1fc36c233a838a844a62e5b0d1f3171cd994808
takagi3 <satoshi.takagi@pm.me>
2024-04-20 00:47:29 +0900

    2024/04/20

--- math-and-algorithm/src/bin/034.rs
+++ math-and-algorithm/src/bin/034.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        xy: [(f64, f64); n],
+    }
+
+    let mut ans: f64 = f64::MAX;
+    for i in 0..n - 1 {
+        for j in i + 1..n {
+            ans = ans.min(((xy[i].0 - xy[j].0).powf(2.0) + (xy[i].1 - xy[j].1).powf(2.0)).sqrt());
+        }
+    }
+
+    println!("{}", ans);
 }
