2e015c90f66f55b4d464b08d17923de95f3110d9
takagi3 <satoshi.takagi@pm.me>
2024-04-23 01:56:56 +0900

    2024/04/23

--- math-and-algorithm/src/bin/036.rs
+++ math-and-algorithm/src/bin/036.rs
@@ -1,3 +1,20 @@
+use std::f64::consts::PI;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: f64,
+        b: f64,
+        h: f64,
+        m: f64,
+    }
+
+    let long: f64 = m * 6.0;
+    let short: f64 = h * 30.0 + m * 0.5;
+    let diff: f64 = (long - short).abs();
+    let angle_degrees: f64 = if diff < 180.0 { diff } else { 360.0 - diff };
+    let angle_radians: f64 = angle_degrees * (PI / 180.0);
+    let ans: f64 = ((a * a + b * b) - 2.0 * a * b * angle_radians.cos()).sqrt();
+
+    println!("{}", ans);
 }
