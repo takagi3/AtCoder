9ace933081c602c4ed8494977b4be8223c8abf7f
takagi3 <satoshi.takagi@pm.me>
2024-04-28 00:16:07 +0900

    2024/04/28

--- math-and-algorithm/src/bin/040.rs
+++ math-and-algorithm/src/bin/040.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [i64; n - 1],
+        m: usize,
+        b: [usize; m],
+    }
+
+    let mut dict: Vec<i64> = vec![0; n];
+    for i in 0..n - 1 {
+        dict[i + 1] = dict[i] + a[i]
+    }
+    let mut ans: i64 = 0;
+    for i in 1..m {
+        ans += (dict[b[i] - 1] - dict[b[i - 1] - 1]).abs()
+    }
+
+    println!("{}", ans);
 }
