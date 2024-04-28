7a83a37d882949d6f377173276a22b1167ba4b14
takagi3 <satoshi.takagi@pm.me>
2024-04-29 02:11:28 +0900

    2024/04/29

--- math-and-algorithm/src/bin/041.rs
+++ math-and-algorithm/src/bin/041.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        t: usize,
+        n: usize,
+        lr: [(usize, usize); n],
+    }
+
+    let mut b: Vec<i32> = vec![0; t + 1];
+    for i in 0..n {
+        b[lr[i].0] += 1;
+        b[lr[i].1] -= 1;
+    }
+    let mut ans: Vec<i32> = vec![0; t];
+    ans[0] = b[0];
+    for i in 1..t {
+        ans[i] = ans[i - 1] + b[i]
+    }
+
+    println!("{}", ans.iter()
+        .map(std::string::ToString::to_string)
+        .collect::<Vec<_>>()
+        .join("\n")
+    );
 }
