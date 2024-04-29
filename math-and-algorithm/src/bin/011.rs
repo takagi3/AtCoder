1acc44e2bda7c056dcb9f42fce7ddde049db432d
takagi3 <satoshi.takagi@pm.me>
2024-04-30 01:21:04 +0900

    2024/04/30

--- math-and-algorithm/src/bin/011.rs
+++ math-and-algorithm/src/bin/011.rs
@@ -1,21 +1,37 @@
+use num_integer::Roots;
 use proconio::input;
 
 fn main() {
     input! {
-        n: u32,
+        n: usize,
     }
 
     let mut ans: Vec<u32> = vec![];
-    for i in 2..=n {
-        let mut is_prime = true;
-        for j in 2..i {
-            if i % j == 0 {
-                is_prime = false;
-                break;
+    // for i in 2..=n {
+    //     let mut is_prime = true;
+    //     for j in 2..i {
+    //         if i % j == 0 {
+    //             is_prime = false;
+    //             break;
+    //         }
+    //     }
+    //     if is_prime {
+    //         ans.push(i)
+    //     }
+    // }
+
+    let mut prime: Vec<bool> = vec![true; n + 1];
+    for i in 2..n.sqrt() + 1 {
+        if prime[i] {
+            for j in (2 * i..=n).step_by(i) {
+                prime[j] = false
             }
         }
-        if is_prime {
-            ans.push(i)
+    }
+
+    for i in 2..=n {
+        if prime[i] {
+            ans.push(i as u32);
         }
     }
 
