bfa80cd3a2122a72b822640ebea54bab3e6f60fd
takagi3 <satoshi.takagi@pm.me>
2024-05-07 01:33:52 +0900

    2024/05/07

--- math-and-algorithm/src/bin/048.rs
+++ math-and-algorithm/src/bin/048.rs
@@ -1,3 +1,41 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        k: usize,
+    }
+
+    let mut graph: Vec<Vec<(usize, usize)>> = vec![vec![]; k];
+    for i in 0..k {
+        for j in 0..10 {
+            if i == 0 && j == 0 { continue; }
+            graph[i].push(((i * 10 + j) % k, j));
+        }
+    }
+
+    let mut dist: Vec<usize> = vec![10_usize.pow(10); k];
+    let mut used: Vec<bool> = vec![false; k];
+    let mut q: BinaryHeap<(Reverse<usize>, usize)> = BinaryHeap::new();
+    q.push((Reverse(0), 0));
+
+    while !q.is_empty() {
+        let pos: usize = q.pop().unwrap().1;
+        if used[pos] == true { continue; }
+        used[pos] = true;
+        for x in &graph[pos] {
+            let to = x.0;
+            let mut cost = dist[pos] + x.1;
+            if pos == 0 {
+                cost = x.1;
+            }
+            if dist[to] > cost {
+                dist[to] = cost;
+                q.push((Reverse(dist[to]), to));
+            }
+        }
+    }
+
+    println!("{}", dist[0]);
 }
