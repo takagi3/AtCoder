749bee8cfcab1669e6d4a0f7ebcf85f4e696227f
takagi3 <satoshi.takagi@pm.me>
2024-05-08 00:33:58 +0900

    2024/05/08

--- math-and-algorithm/src/bin/049.rs
+++ math-and-algorithm/src/bin/049.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+    }
+
+    const MOD: u64 = 1000000007;
+    let mut fibonacci: Vec<u64> = vec![1; n + 1];
+    for i in 3..=n {
+        fibonacci[i] = (fibonacci[i - 1] + fibonacci[i - 2]) % MOD;
+    }
+
+    println!("{}", fibonacci[n]);
 }
