82a747db867b10181a5c78c521af0dc245cf2d39
takagi3 <satoshi.takagi@pm.me>
2024-05-09 01:38:34 +0900

    2024/05/09

--- math-and-algorithm/src/bin/050.rs
+++ math-and-algorithm/src/bin/050.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
+fn mod_pow(a: u64, b: u64, m: u64) -> u64 {
+    let mut p: u64 = a;
+    let mut ret: u64 = 1;
+    for i in 0..30 {
+        if b & (1 << i) != 0 {
+            ret *= p;
+            ret %= m;
+        }
+        p *= p;
+        p %= m;
+    }
+    return ret;
+}
+
 fn main() {
-    todo!();
+    input! {
+        a: u64,
+        b: u64,
+    }
+
+    const MOD: u64 = 1000000007;
+
+    println!("{}", mod_pow(a, b, MOD));
 }
