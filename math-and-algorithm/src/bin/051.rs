36ec3098ebddf0b7ee5080b9bce209f4e33bfd31
takagi3 <satoshi.takagi@pm.me>
2024-05-10 00:12:40 +0900

    2024/05/10

--- math-and-algorithm/src/bin/051.rs
+++ math-and-algorithm/src/bin/051.rs
@@ -1,3 +1,38 @@
+use proconio::input;
+
+fn mod_pow(a: usize, b: usize, m: usize) -> usize {
+    let mut p: usize = a;
+    let mut ret: usize = 1;
+    for i in 0..30 {
+        if b & (1 << i) != 0 {
+            ret *= p;
+            ret %= m;
+        }
+        p *= p;
+        p %= m;
+    }
+    return ret;
+}
+
+fn division(a: usize, b: usize, m: usize) -> usize {
+    return (a * mod_pow(b, m - 2, m) % m) % m;
+}
+
+fn ncr(n: usize, r: usize, f: Vec<usize>, m: usize) -> usize {
+    return division(f[n], f[r] * f[n - r] % m, m);
+}
+
 fn main() {
-    todo!();
+    input! {
+        x: usize,
+        y: usize,
+    }
+
+    const MOD: usize = 1000000007;
+    let mut fact: Vec<usize> = vec![1; 200001];
+    for i in 1..=200000 {
+        fact[i] = 1 * i * fact[i - 1] % MOD;
+    }
+
+    println!("{}", ncr(x + y, y, fact, MOD));
 }
