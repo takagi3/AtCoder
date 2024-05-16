94fc2be9ee94995dae3cf9f1a8bd45ed663e0e75
takagi3 <satoshi.takagi@pm.me>
2024-05-17 00:43:21 +0900

    2024/05/17

--- abc212/src/bin/a.rs
+++ abc212/src/bin/a.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: u32,
+        b: u32,
+    }
+
+    let mut ans: &str;
+    if a == 0 {
+        ans = "Silver";
+    } else if b == 0 {
+        ans = "Gold";
+    } else {
+        ans = "Alloy"
+    }
+
+    println!("{}", ans);
 }
