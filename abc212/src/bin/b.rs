8e37029b6384b1581c88189195854ed374e17574
takagi3 <satoshi.takagi@pm.me>
2024-05-18 04:00:21 +0900

    2024/05/18

--- abc212/src/bin/b.rs
+++ abc212/src/bin/b.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        x: Chars,
+    }
+
+    let mut ans: &str = "Strong";
+    if x[0] == x[1] && x[1] == x[2] && x[2] == x[3] {
+        ans = "Weak";
+    } else if (x[0] as u32 - 47) % 10 == x[1] as u32 - 48
+        && (x[1] as u32 - 47) % 10 == x[2] as u32 - 48
+        && (x[2] as u32 - 47) % 10 == x[3] as u32 - 48 {
+        ans = "Weak";
+    }
+
+    println!("{}", ans);
 }
