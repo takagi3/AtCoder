416ea9463a0036b7238a697e78fec6df03c32750
takagi3 <satoshi.takagi@pm.me>
2024-05-20 00:53:57 +0900

    2024/05/20

--- abc213/src/bin/b.rs
+++ abc213/src/bin/b.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [u32; n],
+    }
+
+    let mut player: Vec<(u32, usize)> = vec![(0, 0); n];
+    for i in 0..n {
+        player[i].0 = a[i];
+        player[i].1 = i + 1;
+    }
+    player.sort();
+
+    println!("{}", player[n - 2].1);
 }
