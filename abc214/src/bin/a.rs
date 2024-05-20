ba320b52db533be1db43df22a2c43062886e4967
takagi3 <satoshi.takagi@pm.me>
2024-05-21 02:47:24 +0900

    2024/05/21

--- abc214/src/bin/a.rs
+++ abc214/src/bin/a.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: u32,
+    }
+
+    let ans: u32;
+    if n <= 125 {
+        ans = 4
+    } else if n >= 212 {
+        ans = 8
+    } else {
+        ans = 6
+    }
+
+    println!("{}", ans);
 }
