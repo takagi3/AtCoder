5f91d45f657a034f03b32fe3e4ec3b08ebe53f23
takagi3 <satoshi.takagi@pm.me>
2024-05-26 01:47:22 +0900

    2024/05/26

--- abc216/src/bin/b.rs
+++ abc216/src/bin/b.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        st: [(String, String); n],
+    }
+
+    let mut ans: &str = "No";
+    for i in 0..n - 1 {
+        for j in i + 1..n {
+            if st[i].0 == st[j].0 && st[i].1 == st[j].1 {
+                ans = "Yes"
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
