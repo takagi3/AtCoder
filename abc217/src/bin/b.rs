f84dcce97fa62a5ead1e83a49bb42d7f88b5a69b
takagi3 <satoshi.takagi@pm.me>
2024-05-28 00:09:27 +0900

    2024/05/28

--- abc217/src/bin/b.rs
+++ abc217/src/bin/b.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        s: [String; 3],
+    }
+
+    let mut ans: Vec<&str> = vec!["ABC", "ARC", "AGC", "AHC"];
+    for i in 0..3 {
+        if let Some(idx) = ans.iter().position(|x| *x == s[i]) {
+            ans.remove(idx);
+        }
+    }
+
+    println!("{}", ans[0]);
 }
