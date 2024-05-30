174b02df7a0290d523425b337da10b1f27cd659f
takagi3 <satoshi.takagi@pm.me>
2024-05-31 00:12:16 +0900

    2024/05/31

--- abc219/src/bin/a.rs
+++ abc219/src/bin/a.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        x: u32,
+    }
+
+    let ans: String;
+    if x < 40 {
+        ans = (40 - x).to_string()
+    } else if x < 70 {
+        ans = (70 - x).to_string()
+    } else if x < 90 {
+        ans = (90 - x).to_string()
+    } else {
+        ans = "expert".to_string()
+    }
+
+    println!("{}", ans);
 }
