236ff2a9dfd40bc9694fe67cee7473cb2742821f
takagi3 <satoshi.takagi@pm.me>
2024-06-01 02:04:31 +0900

    2024/06/01

--- abc219/src/bin/b.rs
+++ abc219/src/bin/b.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s1: String,
+        s2: String,
+        s3: String,
+        t: Chars,
+    }
+
+    let mut ans: String = String::new();
+    for i in 0..t.len() {
+        match t[i] {
+            '1' => ans += &s1,
+            '2' => ans += &s2,
+            '3' => ans += &s3,
+            _ => (),
+        }
+    }
+
+    println!("{}", ans);
 }
