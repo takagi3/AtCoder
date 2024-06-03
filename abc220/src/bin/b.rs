0380aa760e9aa100ac1cb3449d12f6b09a23f8cf
takagi3 <satoshi.takagi@pm.me>
2024-06-03 22:44:21 +0900

    2024/06/03

--- abc220/src/bin/b.rs
+++ abc220/src/bin/b.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        k: i64,
+        mut a: Chars,
+        mut b: Chars,
+    }
+
+    a.reverse();
+    let mut a_num: i64 = 0;
+    for i in 0..a.len() {
+        a_num += (a[i] as i64 - 48) * k.pow(i as u32)
+    }
+    b.reverse();
+    let mut b_num: i64 = 0;
+    for i in 0..b.len() {
+        b_num += (b[i] as i64 - 48) * k.pow(i as u32)
+    }
+    let ans: i64 = a_num * b_num;
+
+    println!("{}", ans);
 }
