5c9b8a721a28f45c16134fd3ced157748507ceaf
takagi3 <satoshi.takagi@pm.me>
2024-06-05 01:25:38 +0900

    2024/06/05

--- abc221/src/bin/b.rs
+++ abc221/src/bin/b.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        mut s: Chars,
+        t: Chars,
+    }
+
+    let mut swap: char;
+    let mut ans: &str = if s == t { "Yes" } else { "No" };
+    for i in 1..s.len() {
+        swap = s[i - 1];
+        s[i - 1] = s[i];
+        s[i] = swap;
+        if s == t {
+            ans = "Yes";
+            break;
+        } else {
+            swap = s[i - 1];
+            s[i - 1] = s[i];
+            s[i] = swap;
+        }
+    }
+
+    println!("{}", ans);
 }
