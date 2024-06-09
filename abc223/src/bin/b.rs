9849efd03ac067fd4d63fe7ad8a1cbc5d2ddb8a5
takagi3 <satoshi.takagi@pm.me>
2024-06-09 21:59:47 +0900

    2024/06/09

--- abc223/src/bin/b.rs
+++ abc223/src/bin/b.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        mut s: Chars,
+    }
+
+    let mut ss: Vec<String> = vec![];
+    let len: usize = s.len();
+    for _ in 0..s.len() {
+        let tmp = s[0];
+        for j in 1..len {
+            s[j - 1] = s[j];
+        }
+        s[len - 1] = tmp;
+        ss.push(s.iter().collect::<String>());
+    }
+
+    let mut ans_min = &ss[0];
+    let mut ans_max = &ss[0];
+    for i in 1..ss.len() {
+        if ans_min > &ss[i] {
+            ans_min = &ss[i]
+        }
+        if ans_max < &ss[i] {
+            ans_max = &ss[i]
+        }
+    }
+
+    println!("{}\n{}", ans_min, ans_max);
 }
