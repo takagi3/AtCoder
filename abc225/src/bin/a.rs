c4667a57613d429487a9b7ee1c4cf8af3584fd73
takagi3 <satoshi.takagi@pm.me>
2024-06-12 01:02:04 +0900

    2024/06/12

--- abc225/src/bin/a.rs
+++ abc225/src/bin/a.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let mut ans: u32 = 6;
+    if s[0] == s[1] && s[1] == s[2] {
+        ans = 1
+    } else if s[0] == s[1] || s[1] == s[2] || s[0] == s[2] {
+        ans = 3
+    }
+
+    println!("{}", ans);
 }
