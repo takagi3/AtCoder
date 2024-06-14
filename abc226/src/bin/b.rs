8fad426482e7d613881d50ed81676793389aef61
takagi3 <satoshi.takagi@pm.me>
2024-06-15 02:44:27 +0900

    2024/06/15

--- abc226/src/bin/b.rs
+++ abc226/src/bin/b.rs
@@ -1,3 +1,20 @@
+use std::collections::HashSet;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+    }
+
+    let mut set: HashSet<Vec<u32>> = HashSet::new();
+    for _ in 0..n {
+        input! {
+            l: usize,
+            a: [u32; l],
+        }
+        set.insert(a);
+    }
+    let ans: usize = set.len();
+
+    println!("{}", ans);
 }
