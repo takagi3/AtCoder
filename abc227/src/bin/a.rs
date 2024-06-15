c615e1b02b03ee20d35495ee5065ad164f0fbdc0
takagi3 <satoshi.takagi@pm.me>
2024-06-16 02:59:26 +0900

    2024/06/16

--- abc227/src/bin/a.rs
+++ abc227/src/bin/a.rs
@@ -1,3 +1,13 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: u32,
+        k: u32,
+        a: u32,
+    }
+
+    let ans: u32 = if (a - 1 + k) % n != 0 { (a - 1 + k) % n } else { n };
+
+    println!("{}", ans);
 }
