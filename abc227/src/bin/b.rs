c66c0e3d2d453a6380717aab0c1e8fe81b2e0790
takagi3 <satoshi.takagi@pm.me>
2024-06-17 00:53:14 +0900

    2024/06/17

--- abc227/src/bin/b.rs
+++ abc227/src/bin/b.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: [u32; n],
+    }
+
+    let mut cnt: u32 = 0;
+    for i in 0..n {
+        'calc: for a in 1..=333 {
+            for b in 1..=333 {
+                if 4 * a * b + 3 * a + 3 * b == s[i] {
+                    cnt += 1;
+                    break 'calc;
+                }
+            }
+        }
+    }
+    let ans: u32 = n as u32 - cnt;
+
+    println!("{}", ans);
 }
