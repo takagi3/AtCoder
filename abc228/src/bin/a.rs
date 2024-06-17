72a02ca3e3e13148cb1829378af9eab6e3ce72f8
takagi3 <satoshi.takagi@pm.me>
2024-06-18 00:58:20 +0900

    2024/06/18

--- abc228/src/bin/a.rs
+++ abc228/src/bin/a.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        s: u32,
+        mut t: u32,
+        x: u32,
+    }
+
+    let ans: &str;
+    if t < s {
+        t += 24
+    }
+    if s <= x && x < t || s <= x + 24 && x + 24 < t {
+        ans = "Yes"
+    } else {
+        ans = "No"
+    }
+
+    println!("{}", ans);
 }
