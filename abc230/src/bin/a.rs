a032bddc11acb309d014263f9e35e6e0e4be93b5
takagi3 <satoshi.takagi@pm.me>
2024-06-22 01:02:01 +0900

    2024/06/22

--- abc230/src/bin/a.rs
+++ abc230/src/bin/a.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: u32,
+    }
+
+    let ans: u32;
+    if n < 42 {
+        ans = n
+    } else {
+        ans = n + 1
+    }
+
+    println!("AGC{0: >03}", ans);
 }
