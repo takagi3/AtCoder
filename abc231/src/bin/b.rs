ead517b9076e85efeba15d949606992a140e0a62
takagi3 <satoshi.takagi@pm.me>
2024-06-25 03:49:55 +0900

    2024/06/25

--- abc231/src/bin/b.rs
+++ abc231/src/bin/b.rs
@@ -1,3 +1,28 @@
+use std::collections::HashMap;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: [String; n],
+    }
+
+    let mut map: HashMap<String, usize> = HashMap::new();
+    for i in 0..n {
+        if map.contains_key(&s[i]) {
+            *map.get_mut(&s[i]).unwrap() += 1;
+        } else {
+            map.insert(s[i].clone(), 1);
+        }
+    }
+    let mut max: usize = 0;
+    let mut ans: &str = "";
+    for (k, v) in map.iter() {
+        if max < *v {
+            ans = k;
+            max = *v;
+        }
+    }
+
+    println!("{}", ans);
 }
