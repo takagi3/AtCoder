9f8da03dfc8fb9ba8d41f69b2e94f80b66d0f329
takagi3 <satoshi.takagi@pm.me>
2024-06-26 00:14:01 +0900

    2024/06/26

--- abc232/src/bin/a.rs
+++ abc232/src/bin/a.rs
@@ -1,3 +1,12 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let ans: i32 = (s[0] as i32 - 48) * (s[2] as i32 - 48);
+
+    println!("{}", ans);
 }
