4a1798930002bcd87daa1730949649045f4a2e9f
takagi3 <satoshi.takagi@pm.me>
2024-06-28 03:11:43 +0900

    2024/06/28

--- abc233/src/bin/a.rs
+++ abc233/src/bin/a.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        x: u32,
+        y: u32,
+    }
+
+    let mut ans: u32 = 0;
+    if x < y {
+        ans = (y - x) / 10;
+        if (y - x) % 10 > 0 {
+            ans += 1
+        }
+    }
+
+    println!("{}", ans);
 }
