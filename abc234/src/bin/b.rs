309e0cd526112352ccceecc7d088c494e9d79377
takagi3 <satoshi.takagi@pm.me>
2024-07-01 02:13:17 +0900

    2024/07/01

--- abc234/src/bin/b.rs
+++ abc234/src/bin/b.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        xy: [(f64, f64); n],
+    }
+
+    let mut ans: f64 = 0.0;
+    for i in 0..n - 1 {
+        for j in i + 1..n {
+            if ((xy[i].0 - xy[j].0).powf(2.0) + (xy[i].1 - xy[j].1).powf(2.0)).sqrt() > ans {
+                ans = ((xy[i].0 - xy[j].0).powf(2.0) + (xy[i].1 - xy[j].1).powf(2.0)).sqrt()
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
