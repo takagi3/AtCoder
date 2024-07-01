4b0305fc6e212cf002885dbe4642b47c2b12aaf2
takagi3 <satoshi.takagi@pm.me>
2024-07-02 03:29:32 +0900

    2024/07/02

--- abc235/src/bin/a.rs
+++ abc235/src/bin/a.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        abc: Chars,
+    }
+
+    let mut ans: i32 = 0;
+    ans += (abc[0] as i32 - 48) * 100 + (abc[1] as i32 - 48) * 10 + (abc[2] as i32 - 48);
+    ans += (abc[1] as i32 - 48) * 100 + (abc[2] as i32 - 48) * 10 + (abc[0] as i32 - 48);
+    ans += (abc[2] as i32 - 48) * 100 + (abc[0] as i32 - 48) * 10 + (abc[1] as i32 - 48);
+
+    println!("{}", ans);
 }
