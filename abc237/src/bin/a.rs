cd2c892a15df459e93905fbac3de36bc955d6c9c
takagi3 <satoshi.takagi@pm.me>
2024-07-06 02:55:34 +0900

    2024/07/06

--- abc237/src/bin/a.rs
+++ abc237/src/bin/a.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+    }
+
+    let mut ans: &str = "No";
+    if -2i64.pow(31) <= n && n < 2i64.pow(31) as i64 {
+        ans = "Yes";
+    }
+
+    println!("{}", ans);
 }
