d84ce04ca827fe192da23f389e1497a594ffe4a7
takagi3 <satoshi.takagi@pm.me>
2024-07-11 01:38:12 +0900

    2024/07/11

--- abc239/src/bin/b.rs
+++ abc239/src/bin/b.rs
@@ -1,3 +1,11 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        x: i128,
+    }
+
+    let ans: i128 = if x < 0 { (x - 9) / 10 } else { x / 10 };
+
+    println!("{}", ans);
 }
