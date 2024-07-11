1a81d327acd8227b000dc073bb09209e3e123a30
takagi3 <satoshi.takagi@pm.me>
2024-07-12 01:16:02 +0900

    2024/07/12

--- abc240/src/bin/a.rs
+++ abc240/src/bin/a.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: u32,
+        b: u32,
+    }
+
+    let mut ans: &str = "No";
+    if b - a == 1 || b - a == 9 {
+        ans = "Yes"
+    }
+
+    println!("{}", ans);
 }
