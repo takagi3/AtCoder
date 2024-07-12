7ee105c67bf6250df4b1086884a82e952c3591e1
takagi3 <satoshi.takagi@pm.me>
2024-07-13 02:57:31 +0900

    2024/07/13

--- abc240/src/bin/b.rs
+++ abc240/src/bin/b.rs
@@ -1,3 +1,17 @@
+use std::collections::HashSet;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [u32; n],
+    }
+
+    let mut cnt: HashSet<u32> = HashSet::new();
+    for i in 0..n {
+        cnt.insert(a[i]);
+    }
+    let ans: usize = cnt.len();
+
+    println!("{}", ans);
 }
