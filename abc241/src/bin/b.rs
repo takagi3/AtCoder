78620b8a53d115b86fe55a27e7b2b4051b9e5e5b
takagi3 <satoshi.takagi@pm.me>
2024-07-15 03:43:01 +0900

    2024/07/15

--- abc241/src/bin/b.rs
+++ abc241/src/bin/b.rs
@@ -1,3 +1,31 @@
+use im_rc::HashMap;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        a: [usize; n],
+        b: [usize; m],
+    }
+
+    let mut ans: &str = "Yes";
+    let mut cnt: HashMap<usize, u32> = HashMap::new();
+    for i in 0..n {
+        if cnt.get(&a[i]) == None {
+            cnt.insert(a[i], 1);
+        } else {
+            cnt[&a[i]] += 1
+        }
+    }
+    for i in 0..m {
+        if cnt.get(&b[i]) == None || cnt[&b[i]] == 0 {
+            ans = "No";
+            break;
+        } else {
+            cnt[&b[i]] -= 1
+        }
+    }
+
+    println!("{}", ans);
 }
