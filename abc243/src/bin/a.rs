2c68ec6c188ee097342706be5a3147e42b078ff7
takagi3 <satoshi.takagi@pm.me>
2024-07-18 02:19:08 +0900

    2024/07/18

--- abc243/src/bin/a.rs
+++ abc243/src/bin/a.rs
@@ -1,3 +1,36 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        mut v: i32,
+        a: i32,
+        b: i32,
+        c: i32,
+    }
+
+    let ans: char;
+    loop {
+        if v >= a {
+            v -= a
+        } else {
+            ans = 'F';
+            break;
+        }
+
+        if v >= b {
+            v -= b
+        } else {
+            ans = 'M';
+            break;
+        }
+
+        if v >= c {
+            v -= c
+        } else {
+            ans = 'T';
+            break;
+        }
+    }
+
+    println!("{}", ans);
 }
