edf40e74c083911b35431d2379a63b8a1aa22816
takagi3 <satoshi.takagi@pm.me>
2024-07-21 01:07:51 +0900

    2024/07/21

--- abc244/src/bin/b.rs
+++ abc244/src/bin/b.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        t: Chars,
+    }
+
+    let dx: Vec<i32> = vec![1, 0, -1, 0];
+    let dy: Vec<i32> = vec![0, -1, 0, 1];
+    let mut dt: usize = 0;
+    let mut ans: (i32, i32) = (0, 0);
+    for i in 0..n {
+        if t[i] == 'S' {
+            ans.0 += dx[dt];
+            ans.1 += dy[dt];
+        } else if t[i] == 'R' {
+            dt = (dt + 1) % 4
+        }
+    }
+
+    println!("{} {}", ans.0, ans.1);
 }
