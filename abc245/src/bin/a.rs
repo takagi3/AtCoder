cd27aa2fb8ff9174c2b41433d6eb2b6315bdc8c1
takagi3 <satoshi.takagi@pm.me>
2024-07-22 01:52:46 +0900

    2024/07/22

--- abc245/src/bin/a.rs
+++ abc245/src/bin/a.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: u32,
+        b: u32,
+        c: u32,
+        d: u32,
+    }
+
+    let mut ans: &str = "Aoki";
+    if a < c {
+        ans = "Takahashi"
+    } else if a == c {
+        if b <= d {
+            ans = "Takahashi"
+        }
+    }
+
+    println!("{}", ans);
 }
