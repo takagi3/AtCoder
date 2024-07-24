03fd27494144d5a6fdee22e5b3f54dc0d1464ba4
takagi3 <satoshi.takagi@pm.me>
2024-07-25 01:40:59 +0900

    2024/07/25

--- abc246/src/bin/b.rs
+++ abc246/src/bin/b.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: f64,
+        b: f64,
+    }
+
+    let c: f64 = (a * a + b * b).sqrt();
+    let x: f64 = a / c;
+    let y: f64 = b / c;
+
+    println!("{} {}", x, y);
 }
