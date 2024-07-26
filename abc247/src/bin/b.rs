3bd6906db635a3b757575dd44a353e8498f68173
takagi3 <satoshi.takagi@pm.me>
2024-07-27 02:22:49 +0900

    2024/07/27

--- abc247/src/bin/b.rs
+++ abc247/src/bin/b.rs
@@ -1,3 +1,36 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        st: [(String, String); n],
+    }
+
+    let mut name: Vec<String> = vec![];
+    for i in 0..n {
+        name.push(st[i].0.clone());
+        if st[i].0 != st[i].1 {
+            name.push(st[i].1.clone());
+        }
+    }
+    let mut ok: bool = true;
+    for i in 0..n {
+        let mut cnt_s: usize = 0;
+        let mut cnt_t: usize = 0;
+        for j in 0..name.len() {
+            if st[i].0 == name[j] {
+                cnt_s += 1
+            }
+            if st[i].1 == name[j] {
+                cnt_t += 1
+            }
+        }
+        if cnt_s > 1 && cnt_t > 1 {
+            ok = false;
+            break;
+        }
+    }
+    let ans: &str = if ok { "Yes" } else { "No" };
+
+    println!("{}", ans);
 }
