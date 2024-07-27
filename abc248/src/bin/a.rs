cdddb00d4b3ee2a071bdb2566971f1a74218bdb1
takagi3 <satoshi.takagi@pm.me>
2024-07-28 01:26:19 +0900

    2024/07/28

--- abc248/src/bin/a.rs
+++ abc248/src/bin/a.rs
@@ -1,3 +1,23 @@
+use std::collections::HashSet;
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars
+    }
+
+    let mut set: HashSet<i32> = HashSet::new();
+    for i in 0..9 {
+        set.insert(s[i] as i32 - 48);
+    }
+    let mut ans: i32 = 0;
+    for i in 0..=9 {
+        if !set.contains(&i) {
+            ans = i;
+            break;
+        }
+    }
+
+    println!("{}", ans);
 }
