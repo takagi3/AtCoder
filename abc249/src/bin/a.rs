5edf695b8afcebef7e7e17b4337cde0a2e75864f
takagi3 <satoshi.takagi@pm.me>
2024-07-30 00:46:44 +0900

    2024/07/30

--- abc249/src/bin/a.rs
+++ abc249/src/bin/a.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: u32,
+        b: u32,
+        c: u32,
+        d: u32,
+        e: u32,
+        f: u32,
+        x: u32,
+    }
+
+    let mut dist_t: u32 = x / (a + c) * (a * b);
+    dist_t = if x % (a + c) >= a { dist_t + (a * b) } else { dist_t + x % (a + c) * b };
+    let mut dist_a: u32 = x / (d + f) * (d * e);
+    dist_a = if x % (d + f) >= d { dist_a + (d * e) } else { dist_a + x % (d + f) * e };
+    let mut ans: &str = "Draw";
+    if dist_t > dist_a {
+        ans = "Takahashi"
+    } else if dist_t < dist_a {
+        ans = "Aoki"
+    }
+
+    println!("{}", ans);
 }
