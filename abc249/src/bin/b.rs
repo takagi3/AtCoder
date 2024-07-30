7eec8e646e82bb233e6ff114e758b9afe752badf
takagi3 <satoshi.takagi@pm.me>
2024-07-31 00:27:47 +0900

    2024/07/31

--- abc249/src/bin/b.rs
+++ abc249/src/bin/b.rs
@@ -1,3 +1,33 @@
+use std::collections::HashSet;
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let mut set: HashSet<char> = HashSet::new();
+    let mut ans: &str = "Yes";
+    let mut upper: bool = false;
+    let mut lower: bool = false;
+    for i in 0..s.len() {
+        if s[i].is_uppercase() {
+            upper = true;
+        }
+        if s[i].is_lowercase() {
+            lower = true;
+        }
+        if set.contains(&s[i]) {
+            ans = "No";
+            break;
+        } else {
+            set.insert(s[i]);
+        }
+    }
+    if !upper || !lower {
+        ans = "No"
+    }
+
+    println!("{}", ans);
 }
