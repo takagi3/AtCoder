e969d25712b46b7d7decaf7664915c7888804e0a
takagi3 <satoshi.takagi@pm.me>
2024-08-03 02:03:15 +0900

    2024/08/03

--- abc251/src/bin/a.rs
+++ abc251/src/bin/a.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        s: String,
+    }
+
+    let mut ans: String = String::new();
+    for _ in 0..6 / s.len() {
+        ans.push_str(&s)
+    }
+
+    println!("{}", ans);
 }
