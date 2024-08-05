98937f6398d3100b68190049ffb825f2b1275860
takagi3 <satoshi.takagi@pm.me>
2024-08-06 01:04:33 +0900

    2024/08/06

--- abc252/src/bin/b.rs
+++ abc252/src/bin/b.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [u32; n],
+        b: [u32; k],
+    }
+
+    let mut max: u32 = 0;
+    for i in 0..n {
+        if a[i] > max {
+            max = a[i]
+        }
+    }
+    let mut ans: &str = "No";
+    for i in 0..n {
+        if a[i] == max && b.contains(&(i as u32 + 1)) {
+            ans = "Yes"
+        }
+    }
+
+    println!("{}", ans);
 }
