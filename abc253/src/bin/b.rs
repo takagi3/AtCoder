beb5a693df94b1f69b2e34c16813530d415cc7db
takagi3 <satoshi.takagi@pm.me>
2024-08-08 01:29:07 +0900

    2024/08/08

--- abc253/src/bin/b.rs
+++ abc253/src/bin/b.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        h: usize,
+        w: usize,
+        s: [Chars; h],
+    }
+
+    let mut ans: i32 = 0;
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
+    for i in 0..h {
+        for j in 0..w {
+            if s[i][j] == 'o' {
+                if ans == 0 {
+                    x = j as i32;
+                    y = i as i32;
+                    ans = 1;
+                } else {
+                    ans = (x - j as i32).abs() + (y - i as i32).abs()
+                }
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
