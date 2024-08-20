673699e2fd87dc9675026c37405c6e284ce8c0bf
takagi3 <satoshi.takagi@pm.me>
2024-08-21 02:58:01 +0900

    2024/08/21

--- abc255/src/bin/b.rs
+++ abc255/src/bin/b.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; k],
+        xy: [(f64, f64); n],
+    }
+
+    let mut dist: Vec<f64> = vec![1000000.0; n];
+    for i in 0..k {
+        for j in 0..n {
+            if j == a[i] - 1 {
+                dist[j] = 0.0;
+                continue;
+            }
+            let current_dist: f64 = ((xy[j].0 - xy[a[i] - 1].0).powf(2.0) + (xy[j].1 - xy[a[i] - 1].1).powf(2.0)).sqrt();
+            dist[j] = if dist[j] > current_dist { current_dist } else { dist[j] };
+        }
+    }
+    let mut ans: f64 = 0.0;
+    for i in 0..n {
+        ans = if ans < dist[i] { dist[i] } else { ans };
+    }
+
+    println!("{}", ans);
 }
