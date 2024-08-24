5a749dc23332cff5f5c3e8d0bf2d70b1e211d1b2
takagi3 <satoshi.takagi@pm.me>
2024-08-24 16:22:42 +0900

    2024/08/24

--- abc257/src/bin/a.rs
+++ abc257/src/bin/a.rs
@@ -1,3 +1,12 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: u32,
+        x: u32,
+    }
+
+    let ans: char = ((x - 1) / n + 65) as u8 as char;
+
+    println!("{}", ans);
 }
