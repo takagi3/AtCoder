6a6b3b5db9c5deb0e30a0971308fa474160c09b6
takagi3 <satoshi.takagi@pm.me>
2024-08-29 02:13:30 +0900

    2024/08/29

--- abc259/src/bin/b.rs
+++ abc259/src/bin/b.rs
@@ -1,3 +1,18 @@
+use std::f64::consts::PI;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: f64,
+        b: f64,
+        d: f64,
+    }
+
+    let dist: f64 = (a.powf(2.0) + b.powf(2.0)).sqrt();
+    let rad: f64 = b.atan2(a);
+    let moved: f64 = d * PI / 180.0;
+    let x: f64 = dist * (rad + moved).cos();
+    let y: f64 = dist * (rad + moved).sin();
+
+    println!("{} {}", x, y);
 }
