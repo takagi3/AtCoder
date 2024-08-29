750c86c114eecba2e25684ac45f853f372305811
takagi3 <satoshi.takagi@pm.me>
2024-08-30 01:48:43 +0900

    2024/08/30

--- abc260/src/bin/a.rs
+++ abc260/src/bin/a.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let ans: String;
+    if s[0] == s[1] && s[1] == s[2] {
+        ans = String::from("-1")
+    } else {
+        if s[0] == s[1] {
+            ans = String::from(s[2])
+        } else if s[0] == s[2] {
+            ans = String::from(s[1])
+        } else {
+            ans = String::from(s[0])
+        }
+    }
+
+    println!("{}", ans);
 }
