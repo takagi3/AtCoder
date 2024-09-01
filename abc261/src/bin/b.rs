64652e80345ca3b59931bc3dc82948c5ae4b6f3c
takagi3 <satoshi.takagi@pm.me>
2024-09-02 00:17:34 +0900

    2024/09/02

--- abc261/src/bin/b.rs
+++ abc261/src/bin/b.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [Chars; n],
+    }
+
+    let mut ans: &str = "correct";
+    'nest: for i in 0..n {
+        for j in 0..n {
+            if i == j { continue; }
+            if a[i][j] == 'W' && a[j][i] == 'L' {
+                continue;
+            } else if a[i][j] == 'L' && a[j][i] == 'W' {
+                continue;
+            } else if a[i][j] == 'D' && a[j][i] == 'D' {
+                continue;
+            }
+            ans = "incorrect";
+            break 'nest;
+        }
+    }
+
+    println!("{}", ans);
 }
