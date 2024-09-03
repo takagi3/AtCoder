a8de5d3416f8f20631d37882fe6eba6d2c993432
takagi3 <satoshi.takagi@pm.me>
2024-09-04 00:45:00 +0900

    2024/09/04

--- abc262/src/bin/b.rs
+++ abc262/src/bin/b.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        uv: [(usize, usize); m],
+    }
+
+    let mut ans: u32 = 0;
+    let mut graph: Vec<Vec<usize>> = vec![vec![]; n + 1];
+    for i in 0..m {
+        graph[uv[i].0].push(uv[i].1);
+        graph[uv[i].1].push(uv[i].0);
+    }
+    for i in 1..=n {
+        for &j in &graph[i] {
+            if i > j { continue; }
+            for &k in &graph[j] {
+                if j > k { continue; }
+                for &l in &graph[k] {
+                    if i == l { ans += 1 }
+                }
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
