386be5e8a042e46c6a8340511f9ccb1a7f536661
takagi3 <satoshi.takagi@pm.me>
2024-09-05 00:32:22 +0900

    2024/09/05

--- abc263/src/bin/a.rs
+++ abc263/src/bin/a.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: [usize; 5],
+    }
+
+    let mut cnt: Vec<u32> = vec![0; 14];
+    for i in 0..5 {
+        cnt[n[i]] += 1
+    }
+    let mut ans: &str = "Yes";
+    for i in 0..14 {
+        if cnt[i] == 1 || cnt[i] == 4 {
+            ans = "No"
+        }
+    }
+
+    println!("{}", ans);
 }
