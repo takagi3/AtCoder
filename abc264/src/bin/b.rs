78cfdadb25f584f150b73e55f3e3fbb4e6799792
takagi3 <satoshi.takagi@pm.me>
2024-09-08 03:38:50 +0900

    2024/09/08

--- abc264/src/bin/b.rs
+++ abc264/src/bin/b.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        r: usize,
+        c: usize,
+    }
+
+    let size: usize = 15;
+    let mut pattern: Vec<Vec<&str>> = vec![vec![""; size]; size];
+    for i in 0..size {
+        for j in 0..size {
+            let layer: usize = i.min(j).min(size - 1 - i).min(size - 1 - j);
+            if layer % 2 == 0 {
+                pattern[i][j] = "black"
+            } else {
+                pattern[i][j] = "white"
+            }
+        }
+    }
+
+    println!("{}", pattern[r - 1][c - 1]);
 }
