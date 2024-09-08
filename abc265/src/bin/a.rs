0aad15b61b969db1956db37b0a31b8885d91f9b6
takagi3 <satoshi.takagi@pm.me>
2024-09-09 01:04:26 +0900

    2024/09/09

--- abc265/src/bin/a.rs
+++ abc265/src/bin/a.rs
@@ -1,3 +1,13 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        x: u32,
+        y: u32,
+        n: u32,
+    }
+
+    let ans: u32 = if x * 3 < y { x * n } else { n / 3 * y + n % 3 * x };
+
+    println!("{}", ans);
 }
