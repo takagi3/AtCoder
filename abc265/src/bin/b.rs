bd1a0ddb30ed7a437d2e9255cf6a7a5169cc3fb9
takagi3 <satoshi.takagi@pm.me>
2024-09-10 23:24:57 +0900

    2024/09/10

--- abc265/src/bin/b.rs
+++ abc265/src/bin/b.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        mut t: i64,
+        a: [i64; n - 1],
+        xy: [(usize, i64); m],
+    }
+
+    let mut ans: &str = "Yes";
+    let mut next: usize = 0;
+    for i in 0..n - 1 {
+        t -= a[i];
+        if t <= 0 {
+            ans = "No";
+            break;
+        }
+        if next < m && xy[next].0 == i + 2 {
+            t += xy[next].1;
+            next += 1;
+        }
+    }
+
+    println!("{}", ans);
 }
