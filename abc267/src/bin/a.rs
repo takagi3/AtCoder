a27f5052b30d90f871a8ebdde617313811e52d7f
takagi3 <satoshi.takagi@pm.me>
2024-09-13 01:39:55 +0900

    2024/09/13

--- abc267/src/bin/a.rs
+++ abc267/src/bin/a.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        s: String,
+    }
+
+    let ans: u32;
+    match s.as_str() {
+        "Monday" => { ans = 5 }
+        "Tuesday" => { ans = 4 }
+        "Wednesday" => { ans = 3 }
+        "Thursday" => { ans = 2 }
+        "Friday" => { ans = 1 }
+        _ => { ans = 0 }
+    }
+
+    println!("{}", ans);
 }
