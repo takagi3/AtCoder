ff1e5da93278bdd417cf51381f58209f12e466b2
takagi3 <satoshi.takagi@pm.me>
2024-09-18 03:13:56 +0900

    2024/09/18

--- abc269/src/bin/a.rs
+++ abc269/src/bin/a.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: i32,
+        b: i32,
+        c: i32,
+        d: i32,
+    }
+
+    let ans1: i32 = (a + b) * (c - d);
+    let ans2: &str = "Takahashi";
+
+    println!("{}\n{}", ans1, ans2);
 }
