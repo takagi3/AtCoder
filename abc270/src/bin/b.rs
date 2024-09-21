30d7e99d34468a0262824b2a77c1509eee437deb
takagi3 <satoshi.takagi@pm.me>
2024-09-22 02:05:23 +0900

    2024/09/22

--- abc270/src/bin/b.rs
+++ abc270/src/bin/b.rs
@@ -1,3 +1,38 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        x: i32,
+        y: i32,
+        z: i32,
+    }
+
+    let mut ans: i32 = 0;
+    if 0 < x {
+        if x < y || y < 0 {
+            ans += x
+        } else {
+            if z < 0 {
+                ans += -2 * z + x
+            } else if z < y {
+                ans += x
+            } else {
+                ans = -1
+            }
+        }
+    } else {
+        if x > y || y > 0 {
+            ans -= x
+        } else {
+            if z > 0 {
+                ans += 2 * z - x
+            } else if z > y {
+                ans -= x
+            } else {
+                ans = -1
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
