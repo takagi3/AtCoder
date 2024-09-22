3743bf538b777e3e2bde372ace1de516f18be527
takagi3 <satoshi.takagi@pm.me>
2024-09-23 00:15:14 +0900

    2024/09/23

--- abc271/src/bin/a.rs
+++ abc271/src/bin/a.rs
@@ -1,3 +1,12 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: u32
+    }
+
+    let x: String = format!("{:x}", n).to_uppercase();
+    let ans: String = format!("{x:0>2}");
+
+    println!("{}", ans);
 }
