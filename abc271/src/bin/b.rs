f9cc43bc3a7a1bb7b810c107b28420cabffb72d5
takagi3 <satoshi.takagi@pm.me>
2024-09-24 00:36:20 +0900

    2024/09/24

--- abc271/src/bin/b.rs
+++ abc271/src/bin/b.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+use proconio::marker::Usize1;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        q: usize,
+        sequences: [[usize]; n],
+        query: [(Usize1, Usize1); q],
+    }
+
+    for (i, j) in query {
+        println!("{}", sequences[i][j]);
+    }
 }
