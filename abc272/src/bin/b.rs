d79ed3404d2bbd4d4b1417744edfc71177fe3e8c
takagi3 <satoshi.takagi@pm.me>
2024-09-26 00:16:22 +0900

    2024/09/26

--- abc272/src/bin/b.rs
+++ abc272/src/bin/b.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        friends: [[usize]; m],
+    }
+
+    let mut pair: Vec<Vec<bool>> = vec![vec![false; n + 1]; n + 1];
+    for i in 0..m {
+        for &j in &friends[i] {
+            for &k in &friends[i] {
+                pair[j][k] = true
+            }
+        }
+    }
+    let mut ans: &str = "Yes";
+    'out: for i in 1..=n {
+        for j in 1..=n {
+            if !pair[i][j] {
+                ans = "No";
+                break 'out;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
