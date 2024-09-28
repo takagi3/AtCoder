757e970fc27de237879b7503334d13d0c92d352e
takagi3 <satoshi.takagi@pm.me>
2024-09-29 00:50:56 +0900

    2024/09/29

--- abc273/src/bin/b.rs
+++ abc273/src/bin/b.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        mut x: f64,
+        k: f64,
+    }
+
+    for i in 1..=k as i64 {
+        x /= 10f64.powf(i as f64);
+        x = x.round();
+        x *= 10f64.powf(i as f64);
+    }
+    let ans: i64 = x as i64;
+
+    println!("{}", ans);
 }
