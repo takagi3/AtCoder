a608a130351f12bb2f3036a94b798a8e38aa95d8
takagi3 <satoshi.takagi@pm.me>
2024-10-02 01:45:24 +0900

    2024/10/02

--- abc275/src/bin/a.rs
+++ abc275/src/bin/a.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        h: [u32; n],
+    }
+
+    let mut ans: usize = 0;
+    let mut max: u32 = 0;
+    for i in 0..n {
+        if max < h[i] {
+            ans = i + 1;
+            max = h[i];
+        }
+    }
+
+    println!("{}", ans);
 }
