69662ba6a753444c316ad7027fd8eb98086dec55
takagi3 <satoshi.takagi@pm.me>
2024-10-03 00:11:25 +0900

    2024/10/03

--- abc275/src/bin/b.rs
+++ abc275/src/bin/b.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+type Mint = ac_library::ModInt998244353;
+
 fn main() {
-    todo!();
+    input! {
+        a: Mint,
+        b: Mint,
+        c: Mint,
+        d: Mint,
+        e: Mint,
+        f: Mint,
+    }
+
+    let ans: Mint = a * b * c - d * e * f;
+
+    println!("{}", ans);
 }
