25b2572cfcfb729490656972538ae9478e831b8f
takagi3 <satoshi.takagi@pm.me>
2024-10-04 00:08:52 +0900

    2024/10/04

--- abc276/src/bin/a.rs
+++ abc276/src/bin/a.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let mut ans: i32 = -1;
+    for i in 0..s.len() {
+        if s[i] == 'a' {
+            ans = i as i32 + 1;
+        }
+    }
+
+    println!("{}", ans);
 }
