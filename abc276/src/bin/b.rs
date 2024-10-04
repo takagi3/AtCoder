5ba194710f05e2f1b17d0feeae28f663e5faf2ee
takagi3 <satoshi.takagi@pm.me>
2024-10-05 01:01:58 +0900

    2024/10/05

--- abc276/src/bin/b.rs
+++ abc276/src/bin/b.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        ab: [(usize, usize); m],
+    }
+
+    let mut ans: Vec<Vec<usize>> = vec![vec![]; n];
+    for i in 0..m {
+        ans[ab[i].0 - 1].push(ab[i].1);
+        ans[ab[i].1 - 1].push(ab[i].0);
+    }
+    for i in 0..n {
+        print!("{} ", ans[i].len());
+        ans[i].sort();
+        println!("{}", ans[i].iter()
+            .map(ToString::to_string)
+            .collect::<Vec<_>>()
+            .join(" ")
+        );
+    }
 }
