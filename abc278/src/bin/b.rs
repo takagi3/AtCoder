10f0f4d3664bbc096c1e904a1fa920a09b3f8f89
takagi3 <satoshi.takagi@pm.me>
2024-10-09 00:15:12 +0900

    2024/10/09

--- abc278/src/bin/b.rs
+++ abc278/src/bin/b.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        h: u32,
+        mut m: u32,
+    }
+
+    let mut ans: (u32, u32) = (0, 0);
+
+    'nest: for i in h..h + 24 {
+        for j in m..m + 60 {
+            if j == 60 {
+                m = 0;
+                break;
+            }
+            if i % 24 % 10 <= 5 && i % 24 / 10 < 2 || i % 24 / 10 == 2 && j / 10 <= 3 {
+                ans.0 = i % 24;
+                ans.1 = j;
+                break 'nest;
+            }
+        }
+    }
+
+    println!("{} {}", ans.0, ans.1);
 }
