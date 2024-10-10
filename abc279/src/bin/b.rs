6b686220399aeab357216a4541c4fbe343c056c8
takagi3 <satoshi.takagi@pm.me>
2024-10-11 01:19:52 +0900

    2024/10/11

--- abc279/src/bin/b.rs
+++ abc279/src/bin/b.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+        t: Chars,
+    }
+
+    let mut ans: &str = "No";
+    if s.len() >= t.len() {
+        'nest: for i in 0..=s.len() - t.len() {
+            for j in 0..t.len() {
+                if s[i + j] != t[j] {
+                    break;
+                }
+                if j == t.len() - 1 {
+                    ans = "Yes";
+                    break 'nest;
+                }
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
