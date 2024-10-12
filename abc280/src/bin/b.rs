758ba77ea38ebf13c517553c073c8f650e10e817
takagi3 <satoshi.takagi@pm.me>
2024-10-13 00:13:09 +0900

    2024/10/13

--- abc280/src/bin/b.rs
+++ abc280/src/bin/b.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: [i32; n],
+    }
+
+    let mut ans: Vec<i32> = vec![0; n];
+    ans[0] = s[0];
+    for i in 1..n {
+        ans[i] = s[i] - s[i - 1]
+    }
+
+    println!("{}", ans.iter()
+        .map(ToString::to_string)
+        .collect::<Vec<_>>()
+        .join(" ")
+    );
 }
