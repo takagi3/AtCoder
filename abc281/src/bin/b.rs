68d9e9573372e639a8b68850b19ec5e2b3a8d3fe
takagi3 <satoshi.takagi@pm.me>
2024-10-15 01:59:39 +0900

    2024/10/15

--- abc281/src/bin/b.rs
+++ abc281/src/bin/b.rs
@@ -1,3 +1,34 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let mut ans: &str = "Yes";
+    if s.len() != 8 {
+        ans = "No"
+    } else {
+        for i in 0..s.len() {
+            if i == 0 || i == 7 {
+                if s[i] < 'A' || 'Z' < s[i] {
+                    ans = "No";
+                    break;
+                }
+            } else if i == 1 {
+                if s[i] < '1' || '9' < s[i] {
+                    ans = "No";
+                    break;
+                }
+            } else {
+                if s[i] < '0' || '9' < s[i] {
+                    ans = "No";
+                    break;
+                }
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
