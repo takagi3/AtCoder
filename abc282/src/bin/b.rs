ae9a1db234fde44a89df463b244f59c2331b41fd
takagi3 <satoshi.takagi@pm.me>
2024-10-17 23:31:10 +0900

    2024/10/17

--- abc282/src/bin/b.rs
+++ abc282/src/bin/b.rs
@@ -1,3 +1,25 @@
+use std::collections::HashSet;
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        s: [Chars; n],
+    }
+
+    let mut pair: HashSet<(usize, usize)> = HashSet::new();
+    for i in 0..m {
+        for j in 0..n - 1 {
+            for k in j + 1..n {
+                if s[j][i] == 'x' && s[k][i] == 'x' {
+                    pair.insert((j, k));
+                }
+            }
+        }
+    }
+    let ans: usize = n * (n - 1) / 2 - pair.len();
+
+    println!("{}", ans);
 }
