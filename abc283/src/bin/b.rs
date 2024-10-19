4471b15f1d8ee440b7602b3c0639de36863b3c71
takagi3 <satoshi.takagi@pm.me>
2024-10-20 01:09:58 +0900

    2024/10/20

--- abc283/src/bin/b.rs
+++ abc283/src/bin/b.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+use proconio::marker::Usize1;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut a: [u32; n],
+        q: usize,
+    }
+
+    for _ in 0..q {
+        input! {
+            mode: u32,
+        }
+        match mode {
+            1 => {
+                input! {
+                    k: Usize1,
+                    x: u32,
+                }
+                a[k] = x
+            }
+            2 => {
+                input! {
+                    k: Usize1,
+                }
+                println!("{}", a[k]);
+            }
+            _ => continue
+        }
+    }
 }
