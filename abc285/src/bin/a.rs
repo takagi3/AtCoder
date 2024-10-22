0067865c52738c032b5b29aaae79f62e90bbd9b4
takagi3 <satoshi.takagi@pm.me>
2024-10-23 00:41:01 +0900

    2024/10/23

--- abc285/src/bin/a.rs
+++ abc285/src/bin/a.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: u32,
+        b: u32,
+    }
+
+    let ans: &str = if a * 2 == b || a * 2 + 1 == b {
+        "Yes"
+    } else {
+        "No"
+    };
+
+    println!("{}", ans);
 }
