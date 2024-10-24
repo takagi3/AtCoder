74da0bad4449e51def5b1aab92e30e87ea807ff6
takagi3 <satoshi.takagi@pm.me>
2024-10-25 01:04:09 +0900

    2024/10/25

--- abc286/src/bin/a.rs
+++ abc286/src/bin/a.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        p: usize,
+        q: usize,
+        r: usize,
+        s: usize,
+        a: [u32; n],
+    }
+
+    let mut b: Vec<u32> = vec![0; n];
+    for i in 0..n {
+        if p - 1 <= i && i <= q - 1 {
+            b[i] = a[r + i - p]
+        } else if r - 1 <= i && i <= s - 1 {
+            b[i] = a[p + i - r]
+        } else {
+            b[i] = a[i]
+        }
+    }
+
+    println!("{}", b.iter()
+        .map(ToString::to_string)
+        .collect::<Vec<_>>()
+        .join(" ")
+    );
 }
