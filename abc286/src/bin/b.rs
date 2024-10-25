89f01b7ee80d15a5eae8de858fd308153b31e8e4
takagi3 <satoshi.takagi@pm.me>
2024-10-26 00:10:02 +0900

    2024/10/26

--- abc286/src/bin/b.rs
+++ abc286/src/bin/b.rs
@@ -1,3 +1,22 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: Chars,
+    }
+
+    let mut ans: String = String::new();
+    let mut i: usize = 0;
+    while i < n {
+        if i < n - 1 && s[i] == 'n' && s[i + 1] == 'a' {
+            ans.push_str("nya");
+            i += 2;
+        } else {
+            ans.push(s[i]);
+            i += 1;
+        }
+    }
+
+    println!("{}", ans);
 }
