b336c5bc138f5245363d7dfc2debd6630f709118
takagi3 <satoshi.takagi@pm.me>
2024-10-27 02:11:23 +0900

    2024/10/27

--- abc287/src/bin/a.rs
+++ abc287/src/bin/a.rs
@@ -1,3 +1,13 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: [String; n],
+    }
+
+    let cnt: usize = s.iter().filter(|&x| x == "For").count();
+    let ans: &str = if cnt > n / 2 { "Yes" } else { "No" };
+
+    println!("{}", ans);
 }
