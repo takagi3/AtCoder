93e33ba7757dab7ab8b1fdc55f91261cdae08da1
takagi3 <satoshi.takagi@pm.me>
2024-10-28 00:37:12 +0900

    2024/10/28

--- abc287/src/bin/b.rs
+++ abc287/src/bin/b.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        s: [usize; n],
+        t: [usize; m],
+    }
+
+    let mut ans: u32 = 0;
+    for i in 0..n {
+        if t.contains(&(s[i] % 1000)) {
+            ans += 1
+        }
+    }
+
+    println!("{}", ans);
 }
