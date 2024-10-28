565ab59504d9786ca51acaf451bc3525c8a18c85
takagi3 <satoshi.takagi@pm.me>
2024-10-29 02:28:38 +0900

    2024/10/29

--- abc288/src/bin/a.rs
+++ abc288/src/bin/a.rs
@@ -1,3 +1,12 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        ab: [(i64, i64); n],
+    }
+
+    for i in 0..n {
+        println!("{}", ab[i].0 + ab[i].1);
+    }
 }
