7620a7053fe2f19c7bd409a1c48742f0f286a7e6
takagi3 <satoshi.takagi@pm.me>
2024-10-30 02:41:16 +0900

    2024/10/30

--- abc288/src/bin/b.rs
+++ abc288/src/bin/b.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        s: [String; n],
+    }
+
+    let mut ans: Vec<&str> = s.iter().take(k).map(String::as_str).collect();
+    ans.sort();
+
+    println!("{}", ans.join("\n"));
 }
