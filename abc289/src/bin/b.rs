490b9a6c9abebefac0e588d2396dde9322c364e5
takagi3 <satoshi.takagi@pm.me>
2024-11-01 01:31:40 +0900

    2024/11/01

--- abc289/src/bin/b.rs
+++ abc289/src/bin/b.rs
@@ -1,3 +1,32 @@
+use std::collections::VecDeque;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: u32,
+        m: usize,
+        a: [u32; m],
+    }
+
+    let mut ans = Vec::with_capacity(n as usize);
+    let mut que = VecDeque::new();
+    let mut idx = 0;
+
+    for i in 1..=n {
+        if idx < m && i == a[idx] {
+            que.push_back(i);
+            idx += 1;
+        } else {
+            ans.push(i);
+            while let Some(x) = que.pop_back() {
+                ans.push(x);
+            }
+        }
+    }
+
+    println!("{}", ans.iter()
+        .map(ToString::to_string)
+        .collect::<Vec<_>>()
+        .join(" ")
+    );
 }
