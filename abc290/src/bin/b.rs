7fe333573ef72b70a54f64b0820510e5903e041f
takagi3 <satoshi.takagi@pm.me>
2024-11-03 02:28:25 +0900

    2024/11/03

--- abc290/src/bin/b.rs
+++ abc290/src/bin/b.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        _n: usize,
+        k: usize,
+        s: Chars,
+    }
+
+    let mut cnt: usize = 0;
+    let ans: String = s.iter()
+        .map(|&c| {
+            if cnt < k && c == 'o' {
+                cnt += 1;
+                'o'
+            } else {
+                'x'
+            }
+        })
+        .collect();
+
+    println!("{}", ans);
 }
