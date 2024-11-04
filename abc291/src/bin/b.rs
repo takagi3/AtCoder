7aaf3af21146452d2e8a9cd99ba24a20af03b9ea
takagi3 <satoshi.takagi@pm.me>
2024-11-05 00:44:32 +0900

    2024/11/05

--- abc291/src/bin/b.rs
+++ abc291/src/bin/b.rs
@@ -1,3 +1,13 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut x: [i32; 5 * n],
+    }
+
+    x.sort();
+    let total: f64 = x[n..4 * n].iter().map(|&v| v as f64).sum();
+
+    println!("{}", total / (3 * n) as f64);
 }
