706b7461efbfc9bc515af92a38cc2799cf456b4c
takagi3 <satoshi.takagi@pm.me>
2024-11-07 00:19:45 +0900

    2024/11/07

--- abc292/src/bin/b.rs
+++ abc292/src/bin/b.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+use proconio::marker::Usize1;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        q: usize,
+        events: [(usize, Usize1); q],
+    }
+
+    let mut cards = vec![0; n];
+    let ans: Vec<_> = events
+        .into_iter()
+        .filter_map(|(c, x)| match c {
+            1 => {
+                cards[x] += 1;
+                None
+            }
+            2 => {
+                cards[x] += 2;
+                None
+            }
+            3 => Some(if cards[x] >= 2 { "Yes" } else { "No" }),
+            _ => None,
+        })
+        .collect();
+
+    println!("{}", ans.join("\n"));
 }
