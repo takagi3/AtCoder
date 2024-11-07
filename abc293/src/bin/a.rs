29859cbcb2759ace09ae7bf291e2f925e07e4d63
takagi3 <satoshi.takagi@pm.me>
2024-11-08 00:43:12 +0900

    2024/11/08

--- abc293/src/bin/a.rs
+++ abc293/src/bin/a.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let ans: String = s.chunks(2)
+        .flat_map(|pair| pair.iter().rev())
+        .collect();
+
+    println!("{}", ans);
 }
