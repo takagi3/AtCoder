005cf62f68feb9abef0fee3b340610115140b9f0
takagi3 <satoshi.takagi@pm.me>
2024-11-09 02:46:50 +0900

    2024/11/09

--- abc293/src/bin/b.rs
+++ abc293/src/bin/b.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+use proconio::marker::Usize1;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [Usize1; n],
+    }
+
+    let mut call = vec![true; n];
+    for i in 0..n {
+        if call[i] {
+            call[a[i]] = false;
+        }
+    }
+    let ans: Vec<usize> = call.iter()
+        .enumerate()
+        .filter_map(|(i, &called)| if called { Some(i + 1) } else { None })
+        .collect();
+
+    println!("{}", ans.len());
+    println!("{}", ans.iter()
+        .map(ToString::to_string)
+        .collect::<Vec<_>>()
+        .join(" ")
+    );
 }
