ba67a1a5655258f6dba8c80a80df6e83473b7322
takagi3 <satoshi.takagi@pm.me>
2024-11-12 00:16:15 +0900

    2024/11/12

--- abc295/src/bin/a.rs
+++ abc295/src/bin/a.rs
@@ -1,3 +1,18 @@
+use std::collections::HashSet;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        w: [String;n],
+    }
+
+    let words: HashSet<&str> = ["and", "not", "that", "the", "you"].iter().cloned().collect();
+    let ans: &str = if w.iter().any(|word| words.contains(word.as_str())) {
+        "Yes"
+    } else {
+        "No"
+    };
+
+    println!("{}", ans);
 }
