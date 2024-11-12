39e0eeaca5284bccd304b3394bfbf2d330e31892
takagi3 <satoshi.takagi@pm.me>
2024-11-13 01:09:20 +0900

    2024/11/13

--- abc295/src/bin/b.rs
+++ abc295/src/bin/b.rs
@@ -1,3 +1,35 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        r: usize,
+        c: usize,
+        b: [Chars; r],
+    }
+
+    let mut ans = vec![vec!['#'; c]; r];
+    for i in 0..r {
+        for j in 0..c {
+            match b[i][j] {
+                '.' => ans[i][j] = '.',
+                '#' => continue,
+                bomb if bomb.is_digit(10) => {
+                    let range = bomb.to_digit(10).unwrap() as i32;
+                    for k in 0..r {
+                        for l in 0..c {
+                            if (i as i32 - k as i32).abs() + (j as i32 - l as i32).abs() <= range {
+                                ans[k][l] = '.';
+                            }
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+
+    for row in ans {
+        println!("{}", row.iter().collect::<String>());
+    }
 }
