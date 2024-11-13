d6b8d726a7afb519650022b6c4bdda838cb7945b
takagi3 <satoshi.takagi@pm.me>
2024-11-14 00:44:12 +0900

    2024/11/14

--- abc296/src/bin/a.rs
+++ abc296/src/bin/a.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        _n: usize,
+        s: Chars,
+    }
+
+    let ans = if s.windows(2).any(|w| w[0] == w[1]) {
+        "No"
+    } else {
+        "Yes"
+    };
+
+    println!("{}", ans);
 }
