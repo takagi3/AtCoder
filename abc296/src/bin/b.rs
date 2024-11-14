f014af651dee54f4fd870ff1e284473f5ac3e31d
takagi3 <satoshi.takagi@pm.me>
2024-11-15 01:15:18 +0900

    2024/11/15

--- abc296/src/bin/b.rs
+++ abc296/src/bin/b.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: [Chars; 8],
+    }
+
+    let mut ans = String::new();
+    for (i, row) in s.iter().enumerate() {
+        if let Some(j) = row.iter().position(|&c| c == '*') {
+            ans.push((b'a' + j as u8) as char);
+            ans.push((b'8' - i as u8) as char);
+        }
+    }
+
+    println!("{}", ans);
 }
