c7340271767fbb12335300cd681547a826dd4374
takagi3 <satoshi.takagi@pm.me>
2024-11-16 01:42:48 +0900

    2024/11/16

--- abc297/src/bin/a.rs
+++ abc297/src/bin/a.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        d: i32,
+        t:[i32; n],
+    }
+
+    let ans = t.windows(2)
+        .find(|w| w[1] - w[0] <= d)
+        .map(|w| w[1])
+        .unwrap_or(-1);
+
+    println!("{}", ans);
 }
