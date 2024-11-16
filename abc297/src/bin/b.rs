6ea5a79fe90357d9353c8c3883325a4b13363742
takagi3 <satoshi.takagi@pm.me>
2024-11-17 00:38:04 +0900

    2024/11/17

--- abc297/src/bin/b.rs
+++ abc297/src/bin/b.rs
@@ -1,3 +1,42 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let mut first_b_index = None;
+    let mut alternating_b_positions = false;
+    let mut found_k_between_r = false;
+    let mut r_count = 0;
+    for (i, &c) in s.iter().enumerate() {
+        match c {
+            'B' => {
+                if let Some(b_index) = first_b_index {
+                    if b_index % 2 != i % 2 {
+                        alternating_b_positions = true;
+                    }
+                } else {
+                    first_b_index = Some(i);
+                }
+            }
+            'R' => {
+                r_count += 1;
+            }
+            'K' => {
+                if r_count == 1 {
+                    found_k_between_r = true;
+                }
+            }
+            _ => {}
+        }
+    }
+    let ans = if alternating_b_positions && found_k_between_r {
+        "Yes"
+    } else {
+        "No"
+    };
+
+    println!("{}", ans);
 }
