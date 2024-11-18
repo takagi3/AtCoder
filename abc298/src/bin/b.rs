2c02880f38e72262e84c62543f3fcc6bcfaafcef
takagi3 <satoshi.takagi@pm.me>
2024-11-19 01:07:04 +0900

    2024/11/19

--- abc298/src/bin/b.rs
+++ abc298/src/bin/b.rs
@@ -1,3 +1,40 @@
+use proconio::input;
+
+fn rotate(matrix: &mut Vec<Vec<u32>>) {
+    let n: usize = matrix.len();
+    for i in 0..n {
+        for j in i + 1..n {
+            matrix[i][j] ^= matrix[j][i];
+            matrix[j][i] ^= matrix[i][j];
+            matrix[i][j] ^= matrix[j][i];
+        }
+    }
+    for row in matrix.iter_mut() {
+        row.reverse();
+    }
+}
+
+fn can_match(a: &Vec<Vec<u32>>, b: &Vec<Vec<u32>>) -> bool {
+    a.iter()
+        .zip(b.iter())
+        .all(|(row_a, row_b)| row_a.iter().zip(row_b.iter()).all(|(&a, &b)| a == 0 || b == 1))
+}
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut a: [[u32; n]; n],
+        b: [[u32; n]; n],
+    }
+
+    let mut ans: &str = "No";
+    for _ in 0..4 {
+        if can_match(&a, &b) {
+            ans = "Yes";
+            break;
+        }
+        rotate(&mut a);
+    }
+
+    println!("{}", ans);
 }
