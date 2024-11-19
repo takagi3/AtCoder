7d4fc344b46736bedef07e4bdde30528501f90f4
takagi3 <satoshi.takagi@pm.me>
2024-11-20 00:32:33 +0900

    2024/11/20

--- abc299/src/bin/a.rs
+++ abc299/src/bin/a.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        _n: usize,
+        s: Chars,
+    }
+
+    let mut ans = "out";
+    let mut ok = false;
+    for c in s.iter() {
+        match c {
+            '|' => ok ^= true,
+            '*' if ok => {
+                ans = "in";
+                break;
+            }
+            _ => {}
+        }
+    }
+
+    println!("{}", ans);
 }
