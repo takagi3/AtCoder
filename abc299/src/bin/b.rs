a20e88581735eb255a5ccb7109221be8f1a21da9
takagi3 <satoshi.takagi@pm.me>
2024-11-21 00:10:55 +0900

    2024/11/21

--- abc299/src/bin/b.rs
+++ abc299/src/bin/b.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        t: u32,
+        c: [u32; n],
+        r: [u32; n],
+    }
+
+    let ans = if c.contains(&t) {
+        (0..n)
+            .filter(|&i| c[i] == t)
+            .max_by_key(|&i| r[i])
+            .unwrap()
+    } else {
+        (0..n)
+            .filter(|&i| c[i] == c[0])
+            .max_by_key(|&i| r[i])
+            .unwrap()
+    };
+
+    println!("{}", ans + 1);
 }
