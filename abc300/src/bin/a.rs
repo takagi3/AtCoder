7363ce64f387c16d0085222a36e2b6d05af20414
takagi3 <satoshi.takagi@pm.me>
2024-11-22 00:39:06 +0900

    2024/11/22

--- abc300/src/bin/a.rs
+++ abc300/src/bin/a.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: u32,
+        b: u32,
+        c: [u32; n],
+    }
+
+    if let Some(i) = c.iter().position(|&x| x == a + b) {
+        println!("{}", i + 1);
+    }
 }
