981b37fd1c011c628f556a193d0932d8216e7970
takagi3 <satoshi.takagi@pm.me>
2024-11-23 01:18:16 +0900

    2024/11/23

--- abc300/src/bin/b.rs
+++ abc300/src/bin/b.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        h: usize,
+        w: usize,
+        a: [Chars; h],
+        b: [Chars; h],
+    }
+
+    let mut ans: &str = "No";
+    'nest: for i in 0..h {
+        for j in 0..w {
+            if (0..h).all(|k| (0..w).all(|l| a[(i + k) % h][(j + l) % w] == b[k][l])) {
+                ans = "Yes";
+                break 'nest;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
