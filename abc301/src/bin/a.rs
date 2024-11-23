491f10781f4d8d3bebf0a0ce72bf0a68e6afc76c
takagi3 <satoshi.takagi@pm.me>
2024-11-24 01:23:13 +0900

    2024/11/24

--- abc301/src/bin/a.rs
+++ abc301/src/bin/a.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        _n: usize,
+        a: Chars
+    }
+
+    let mut ans: char = 'A';
+    let mut cnt_a: u32 = 0;
+    let mut cnt_t: u32 = 0;
+    for &p in &a {
+        match p {
+            'A' => cnt_a += 1,
+            'T' => cnt_t += 1,
+            _ => {}
+        }
+        if cnt_a > cnt_t {
+            ans = 'A'
+        } else if cnt_a < cnt_t {
+            ans = 'T'
+        }
+    }
+
+    println!("{}", ans);
 }
