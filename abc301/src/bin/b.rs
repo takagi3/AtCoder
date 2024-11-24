3f959683c9b0d9e688ce56bf13d90332e8b2289c
takagi3 <satoshi.takagi@pm.me>
2024-11-25 00:22:32 +0900

    2024/11/25

--- abc301/src/bin/b.rs
+++ abc301/src/bin/b.rs
@@ -1,3 +1,28 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [u32; n],
+    }
+
+    let ans: Vec<u32> = a
+        .windows(2)
+        .flat_map(|w| {
+            if w[0] < w[1] {
+                (w[0]..w[1]).collect::<Vec<_>>()
+            } else {
+                (w[1] + 1..=w[0]).rev().collect::<Vec<_>>()
+            }
+        })
+        .chain(std::iter::once(*a.last().unwrap()))
+        .collect();
+
+    println!(
+        "{}",
+        ans.iter()
+            .map(ToString::to_string)
+            .collect::<Vec<_>>()
+            .join(" ")
+    );
 }
