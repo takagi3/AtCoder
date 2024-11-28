e08f56338e8b3f2ac8738f11968fa728e9744f3b
takagi3 <satoshi.takagi@pm.me>
2024-11-29 01:05:46 +0900

    2024/11/29

--- abc303/src/bin/b.rs
+++ abc303/src/bin/b.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+use std::collections::HashSet;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        a: [[usize; n]; m],
+    }
+
+    let mut adjacency = vec![HashSet::new(); n + 1];
+    for row in &a {
+        row.windows(2).for_each(|w| {
+            adjacency[w[0]].insert(w[1]);
+            adjacency[w[1]].insert(w[0]);
+        });
+    }
+    let total_missing_connections: usize = adjacency
+        .iter()
+        .skip(1)
+        .map(|neighbors| n - 1 - neighbors.len())
+        .sum();
+
+    println!("{}", total_missing_connections / 2);
 }
