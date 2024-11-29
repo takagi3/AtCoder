d3d1da9f688f0470a651267afe99dccb919a17f5
takagi3 <satoshi.takagi@pm.me>
2024-11-30 03:58:17 +0900

    2024/11/30

--- abc304/src/bin/a.rs
+++ abc304/src/bin/a.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut player: [(String, u32); n],
+    }
+
+    let head = player
+        .iter()
+        .enumerate()
+        .min_by_key(|&(_, &(_, value))| value)
+        .map(|(index, _)| index)
+        .unwrap();
+    player.rotate_left(head);
+
+    for (name, _) in player {
+        println!("{}", name);
+    }
 }
