2d9ca36090a07e6fd5bdebeb32bf0722fc562ece
takagi3 <satoshi.takagi@pm.me>
2024-12-01 03:13:28 +0900

    2024/12/01

--- abc304/src/bin/b.rs
+++ abc304/src/bin/b.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+    }
+
+    let digits = n.to_string().len();
+    let ans = if digits > 3 {
+        let round_base = 10_i64.pow((digits - 3) as u32);
+        (n / round_base) * round_base
+    } else {
+        n
+    };
+
+    println!("{}", ans);
 }
