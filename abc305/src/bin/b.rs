2c06223cd9e6eecd7f9a050d29e21d2288e474ed
takagi3 <satoshi.takagi@pm.me>
2024-12-03 01:06:36 +0900

    2024/12/03

--- abc305/src/bin/b.rs
+++ abc305/src/bin/b.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
+const DISTANCES: [i32; 7] = [0, 3, 4, 8, 9, 14, 23];
+
+fn char_to_index(c: char) -> usize {
+    (c as u8 - b'A') as usize
+}
+
 fn main() {
-    todo!();
+    input! {
+        p: char,
+        q: char,
+    }
+
+    let ans = (DISTANCES[char_to_index(p)] - DISTANCES[char_to_index(q)]).abs();
+    println!("{}", ans);
 }
