90c7f064fc5bf7421be169501ee055c51f5a2488
takagi3 <satoshi.takagi@pm.me>
2024-12-05 01:11:24 +0900

    2024/12/05

--- abc306/src/bin/a.rs
+++ abc306/src/bin/a.rs
@@ -1,3 +1,13 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        _n: usize,
+        s: Chars,
+    }
+
+    let ans: String = s.iter().flat_map(|&c| std::iter::repeat(c).take(2)).collect();
+
+    println!("{}", ans);
 }
