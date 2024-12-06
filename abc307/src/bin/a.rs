b044943bd20230ff377d9ce5b0787f60829c7eef
takagi3 <satoshi.takagi@pm.me>
2024-12-07 03:12:36 +0900

    2024/12/07

--- abc307/src/bin/a.rs
+++ abc307/src/bin/a.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [u32; n * 7],
+    }
+
+    let ans = a
+        .chunks(7)
+        .map(|week| week.iter().sum::<u32>())
+        .collect::<Vec<_>>();
+
+    println!(
+        "{}",
+        ans.iter()
+            .map(ToString::to_string)
+            .collect::<Vec<_>>()
+            .join(" ")
+    );
 }
