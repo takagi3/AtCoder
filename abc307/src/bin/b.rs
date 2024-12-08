bd1091e4b92adf71444993a2eca93ffdd0c7b51e
takagi3 <satoshi.takagi@pm.me>
2024-12-09 01:59:59 +0900

    2024/12/09

--- abc307/src/bin/b.rs
+++ abc307/src/bin/b.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: [Chars; n],
+    }
+
+    let mut ans = "No";
+    'nest: for i in 0..n {
+        for j in 0..n {
+            if i != j {
+                let base: Vec<char> = s[i].iter().chain(&s[j]).cloned().collect();
+                if base.iter().eq(base.iter().rev()) {
+                    ans = "Yes";
+                    break 'nest;
+                }
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
