939401ac553b5d64335a9e806812c28015ae995b
takagi3 <satoshi.takagi@pm.me>
2024-12-10 02:41:58 +0900

    2024/12/10

--- abc308/src/bin/a.rs
+++ abc308/src/bin/a.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        s: [u32; 8],
+    }
+
+    let ans: &str = if s.iter().all(|&x| (100..=675).contains(&x) && x % 25 == 0)
+        && s.windows(2).all(|w| w[0] <= w[1])
+    {
+        "Yes"
+    } else {
+        "No"
+    };
+
+    println!("{}", ans);
 }
