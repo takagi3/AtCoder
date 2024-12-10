3cc044096d7e0afafb9f65c0e54061ab815fe3ad
takagi3 <satoshi.takagi@pm.me>
2024-12-11 01:23:36 +0900

    2024/12/11

--- abc308/src/bin/b.rs
+++ abc308/src/bin/b.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+use std::collections::HashMap;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        c: [String; n],
+        d: [String; m],
+        p: [u32; m+1],
+    }
+
+    let price_map: HashMap<_, _> = d.iter().zip(p.iter().skip(1)).collect();
+    let ans: u32 = c.iter()
+        .map(|item| *price_map.get(item).unwrap_or(&&p[0]))
+        .sum();
+
+    println!("{}", ans);
 }
