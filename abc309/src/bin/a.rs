feed80154e72e5cd3bb061b46a96171143c9b6a5
takagi3 <satoshi.takagi@pm.me>
2024-12-12 17:27:24 +0900

    2024/12/12

--- abc309/src/bin/a.rs
+++ abc309/src/bin/a.rs
@@ -1,3 +1,10 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: u32,
+        b: u32,
+    }
+
+    println!("{}", if (a - 1) / 3 == (b - 1) / 3 { "Yes" } else { "No" });
 }
