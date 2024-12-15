c536a3ad3566efc92f68570ceafecbf40b7d4649
takagi3 <satoshi.takagi@pm.me>
2024-12-16 00:45:04 +0900

    2024/12/16

--- abc311/src/bin/a.rs
+++ abc311/src/bin/a.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+use proconio::marker::Chars;
+use std::collections::HashSet;
+
 fn main() {
-    todo!();
+    input! {
+        _n: usize,
+        s: Chars,
+    }
+
+    let mut ans: usize = 0;
+    let mut seen: HashSet<char> = HashSet::new();
+    for (i, &ch) in s.iter().enumerate() {
+        seen.insert(ch);
+        if seen.len() == 3 {
+            ans = i + 1;
+            break;
+        }
+    }
+
+    println!("{}", ans);
 }
