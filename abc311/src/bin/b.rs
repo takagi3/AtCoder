ce76e6408e1946b5067bebc1b61f05fc1bd0c00b
takagi3 <satoshi.takagi@pm.me>
2024-12-17 00:29:12 +0900

    2024/12/17

--- abc311/src/bin/b.rs
+++ abc311/src/bin/b.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        d: usize,
+        s: [Chars; n],
+    }
+
+    let mut ans = 0;
+    let mut current_streak = 0;
+    for i in 0..d {
+        if (0..n).all(|j| s[j][i] == 'o') {
+            current_streak += 1;
+            ans = ans.max(current_streak);
+        } else {
+            current_streak = 0;
+        }
+    }
+
+    println!("{}", ans);
 }
