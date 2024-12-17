cf09c3f5493e25367c044bea137473f83a8cf669
takagi3 <satoshi.takagi@pm.me>
2024-12-18 02:48:07 +0900

    2024/12/18

--- abc312/src/bin/a.rs
+++ abc312/src/bin/a.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        s: String,
+    }
+
+    const CHORDS: [&str; 7] = ["ACE", "BDF", "CEG", "DFA", "EGB", "FAC", "GBD"];
+    let ans = if CHORDS.iter().any(|&chord| chord == s) {
+        "Yes"
+    } else {
+        "No"
+    };
+
+    println!("{}", ans);
 }
