b79e93d70de22aa4f72ffc48fbd5ffb30118c91a
takagi3 <satoshi.takagi@pm.me>
2024-12-19 03:36:03 +0900

    2024/12/19

--- abc312/src/bin/b.rs
+++ abc312/src/bin/b.rs
@@ -1,3 +1,43 @@
+use proconio::input;
+
+fn takcode(y: usize, x: usize, s: &[Vec<char>]) -> bool {
+    for (i, j) in (0..3).flat_map(|i| (0..3).map(move |j| (i, j))) {
+        if s[y + i][x + j] == '.' || s[y + 6 + i][x + 6 + j] == '.' {
+            return false;
+        }
+    }
+    let ranges = [
+        (3..4, 0..4),
+        (0..4, 3..4),
+        (5..6, 5..9),
+        (5..9, 5..6),
+    ];
+    for (i_range, j_range) in &ranges {
+        for i in i_range.clone() {
+            for j in j_range.clone() {
+                if s[y + i][x + j] == '#' {
+                    return false;
+                }
+            }
+        }
+    }
+    true
+}
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        s: [String; n],
+    }
+
+    let s: Vec<Vec<char>> = s.into_iter().map(|row| row.chars().collect()).collect();
+
+    for i in 0..=n - 9 {
+        for j in 0..=m - 9 {
+            if takcode(i, j, &s) {
+                println!("{} {}", i + 1, j + 1);
+            }
+        }
+    }
 }
