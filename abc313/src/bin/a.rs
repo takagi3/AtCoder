f7691ddd0d29d6362b94742f231805e84d3151a5
takagi3 <satoshi.takagi@pm.me>
2024-12-20 02:32:57 +0900

    2024/12/20

--- abc313/src/bin/a.rs
+++ abc313/src/bin/a.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        p: [u32; n],
+    }
+
+    let ans = if n == 1 {
+        0
+    } else {
+        let max_score = *p.iter().skip(1).max().unwrap();
+        if max_score < p[0] { 0 } else { max_score - p[0] + 1 }
+    };
+
+    println!("{}", ans);
 }
