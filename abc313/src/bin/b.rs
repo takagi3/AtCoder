2b7560dc98ef4077b462db0282bdeed320acd558
takagi3 <satoshi.takagi@pm.me>
2024-12-21 02:50:59 +0900

    2024/12/21

--- abc313/src/bin/b.rs
+++ abc313/src/bin/b.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+use std::collections::HashSet;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        ab: [(usize, usize); m],
+    }
+
+    let set: HashSet<usize> = ab.iter().map(|&(_, b)| b).collect();
+    let ans = if set.len() == n - 1 {
+        (1..=n)
+            .find(|&i| !set.contains(&i))
+            .map_or(-1, |v| v as i32)
+    } else {
+        -1
+    };
+
+    println!("{}", ans);
 }
