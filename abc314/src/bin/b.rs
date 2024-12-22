9a17d900c1d0e2b8e7c1b75375bd6bc5bd88a276
takagi3 <satoshi.takagi@pm.me>
2024-12-23 03:01:04 +0900

    2024/12/23

--- abc314/src/bin/b.rs
+++ abc314/src/bin/b.rs
@@ -1,3 +1,43 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+    }
+    let mut data = Vec::new();
+    for _ in 0..n {
+        input! {
+            c: usize,
+            a: [usize; c],
+        }
+        data.push((c, a));
+    }
+    input! {
+        x: usize,
+    }
+
+    let mut min_c = usize::MAX;
+    let mut ans = Vec::new();
+    for (i, (c, a)) in data.into_iter().enumerate() {
+        if a.contains(&x) {
+            if c < min_c {
+                min_c = c;
+                ans.clear();
+            }
+            if c == min_c {
+                ans.push(i + 1);
+            }
+        }
+    }
+
+    println!("{}", ans.len());
+    if !ans.is_empty() {
+        println!(
+            "{}",
+            ans.iter()
+                .map(|i| i.to_string())
+                .collect::<Vec<_>>()
+                .join(" ")
+        );
+    }
 }
