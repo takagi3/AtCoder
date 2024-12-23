b59647a293ee519112d25c39a19c5921119e5dcf
takagi3 <satoshi.takagi@pm.me>
2024-12-24 02:48:55 +0900

    2024/12/24

--- abc315/src/bin/a.rs
+++ abc315/src/bin/a.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+use proconio::marker::Chars;
+use std::collections::HashSet;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let vowels: HashSet<char> = HashSet::from(['a', 'i', 'u', 'e', 'o']);
+    let ans: String = s.into_iter().filter(|&c| !vowels.contains(&c)).collect();
+
+    println!("{}", ans);
 }
