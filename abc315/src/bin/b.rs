412b0cfa6d73214233adab19e717a2cb6f4352b0
takagi3 <satoshi.takagi@pm.me>
2024-12-25 03:33:42 +0900

    2024/12/25

--- abc315/src/bin/b.rs
+++ abc315/src/bin/b.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        m: usize,
+        d: [isize; m],
+    }
+
+    let year: isize = d.iter().sum();
+    let mut cnt = (year + 1) / 2;
+    let mut ans = (1, 1);
+    for (i, &days) in d.iter().enumerate() {
+        if cnt <= days {
+            ans = (i + 1, cnt as usize);
+            break;
+        }
+        cnt -= days;
+    }
+
+    println!("{} {}", ans.0, ans.1);
 }
