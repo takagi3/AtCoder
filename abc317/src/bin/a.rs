3daff1a41b7a8260f77a77f983b6408d1046d4d0
takagi3 <satoshi.takagi@pm.me>
2024-12-26 02:33:25 +0900

    2024/12/26

--- abc317/src/bin/a.rs
+++ abc317/src/bin/a.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        h: u32,
+        x: u32,
+        p: [u32; n],
+    }
+
+    let ans = p
+        .iter()
+        .enumerate()
+        .filter(|&(_, &pi)| h + pi >= x)
+        .min_by_key(|&(_, &pi)| pi)
+        .map(|(i, _)| i + 1)
+        .unwrap_or(0);
+
+    println!("{}", ans);
 }
