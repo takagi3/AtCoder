fec70372a8f3e61719933bc40452c38de8fb2188
takagi3 <satoshi.takagi@pm.me>
2024-12-27 01:33:41 +0900

    2024/12/27

--- abc317/src/bin/b.rs
+++ abc317/src/bin/b.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+use std::collections::HashSet;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [u32; n],
+    }
+
+    let mut ans: u32 = 0;
+    if let (Some(&min), Some(&max)) = (a.iter().min(), a.iter().max()) {
+        let set: HashSet<_> = a.iter().cloned().collect();
+        for i in min..=max {
+            if !set.contains(&i) {
+                ans = i;
+                break;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
