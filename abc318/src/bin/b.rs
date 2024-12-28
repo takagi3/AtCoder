89226ae411e1435d0a8ec6e8a4b2877d5f27bec1
takagi3 <satoshi.takagi@pm.me>
2024-12-29 04:03:18 +0900

    2024/12/29

--- abc318/src/bin/b.rs
+++ abc318/src/bin/b.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+use std::collections::HashSet;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        rectangles: [(usize, usize, usize, usize); n],
+    }
+
+    let sheets: HashSet<_> = rectangles
+        .iter()
+        .flat_map(|&(a, b, c, d)| (a..b).flat_map(move |j| (c..d).map(move |k| (j, k))))
+        .collect();
+
+    println!("{}", sheets.len());
 }
