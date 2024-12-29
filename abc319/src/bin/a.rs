b84beb0facdc04025a5f449aff7b86093d706ba4
takagi3 <satoshi.takagi@pm.me>
2024-12-30 00:54:33 +0900

    2024/12/30

--- abc319/src/bin/a.rs
+++ abc319/src/bin/a.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+use std::collections::HashMap;
+
 fn main() {
-    todo!();
+    input! {
+        s: String,
+    }
+
+    let player: HashMap<&str, u32> = HashMap::from([
+        ("tourist", 3858),
+        ("ksun48", 3679),
+        ("Benq", 3658),
+        ("Um_nik", 3648),
+        ("apiad", 3638),
+        ("Stonefeang", 3630),
+        ("ecnerwala", 3613),
+        ("mnbvmar", 3555),
+        ("newbiedmy", 3516),
+        ("semiexp", 3481),
+    ]);
+
+    if let Some(rating) = player.get(s.as_str()) {
+        println!("{}", rating);
+    }
 }
