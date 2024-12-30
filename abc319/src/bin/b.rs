3161454fabfe5b6398f10fcf5a0d43c70f118b3c
takagi3 <satoshi.takagi@pm.me>
2024-12-31 03:33:49 +0900

    2024/12/31

--- abc319/src/bin/b.rs
+++ abc319/src/bin/b.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: u32,
+    }
+
+    let ans: String = (0..=n)
+        .map(|i| {
+            (1..=9)
+                .find(|&j| n % j == 0 && i % (n / j) == 0)
+                .map_or('-', |j| (j as u8 + b'0') as char)
+        })
+        .collect();
+
+    println!("{}", ans);
 }
