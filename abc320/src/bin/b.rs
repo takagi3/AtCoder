9bc2079c096f2500da799d57431e4388272d082b
takagi3 <satoshi.takagi@pm.me>
2025-01-03 02:07:49 +0900

    2025/01/03

--- abc320/src/bin/b.rs
+++ abc320/src/bin/b.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let mut ans = 1;
+    for i in 0..s.len() {
+        for j in i + 1..=s.len() {
+            let slice: Vec<char> = s[i..j].to_vec();
+            let reversed_slice: Vec<char> = slice.iter().rev().cloned().collect();
+            if slice == reversed_slice {
+                ans = ans.max(slice.len());
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
