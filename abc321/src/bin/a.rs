48e7f8eb1757d8314d51e904870fdbbe85f847eb
takagi3 <satoshi.takagi@pm.me>
2025-01-04 01:04:16 +0900

    2025/01/04

--- abc321/src/bin/a.rs
+++ abc321/src/bin/a.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        mut n: u32,
+    }
+
+    let result = n
+        .to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect::<Vec<_>>()
+        .windows(2)
+        .all(|w| w[0] > w[1]);
+
+    println!("{}", if result { "Yes" } else { "No" });
 }
