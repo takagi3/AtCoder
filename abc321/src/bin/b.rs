280d464902f159de931e0d59d9c5482577605dfc
takagi3 <satoshi.takagi@pm.me>
2025-01-06 01:54:53 +0900

    2025/01/06

--- abc321/src/bin/b.rs
+++ abc321/src/bin/b.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        x: i32,
+        mut a: [i32; n - 1],
+    }
+
+    let mut ans = -1;
+    for i in 0..=100 {
+        let mut extended = a.clone();
+        extended.push(i);
+        extended.sort_unstable();
+        let sum: i32 = extended[1..n - 1].iter().sum();
+        if sum >= x {
+            ans = i;
+            break;
+        }
+    }
+
+    println!("{}", ans);
 }
