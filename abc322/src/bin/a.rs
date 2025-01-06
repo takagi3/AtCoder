44cf91d83352567705e6da6f37546313ad1f8a7f
takagi3 <satoshi.takagi@pm.me>
2025-01-07 02:42:22 +0900

    2025/01/07

--- abc322/src/bin/a.rs
+++ abc322/src/bin/a.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        _n: usize,
+        s: Chars,
+    }
+
+    let ans = s.windows(3)
+        .position(|w| w == ['A', 'B', 'C'])
+        .map_or(-1, |i| (i + 1) as isize);
+
+    println!("{}", ans);
 }
