053b42bccd5ea94fbb519ae88d18b16c8dc885f0
takagi3 <satoshi.takagi@pm.me>
2025-01-10 01:47:54 +0900

    2025/01/10

--- abc323/src/bin/b.rs
+++ abc323/src/bin/b.rs
@@ -12,9 +12,8 @@ fn main() {
         .enumerate()
         .map(|(i, row)| (row.iter().filter(|&&c| c == 'o').count(), i + 1))
         .collect();
-
     cnt.sort_by(|a, b| b.0.cmp(&a.0).then_with(|| a.1.cmp(&b.1)));
-
     let ans: Vec<String> = cnt.iter().map(|&(_, i)| i.to_string()).collect();
+
     println!("{}", ans.join(" "));
 }
