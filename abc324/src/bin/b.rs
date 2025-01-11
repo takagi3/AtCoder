cdea4a1cb0d3e54b17aee3b1092220e55e8fa489
takagi3 <satoshi.takagi@pm.me>
2025-01-12 01:24:38 +0900

    2025/01/12

--- abc324/src/bin/b.rs
+++ abc324/src/bin/b.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        mut n: usize,
+    }
+
+    while n % 2 == 0 {
+        n /= 2;
+    }
+    while n % 3 == 0 {
+        n /= 3;
+    }
+
+    println!("{}", if n == 1 { "Yes" } else { "No" });
 }
