f75b934c8f5237201d78d0954cfc6765cdf086b6
takagi3 <satoshi.takagi@pm.me>
2025-01-15 03:07:59 +0900

    2025/01/15

--- abc325/src/bin/b.rs
+++ abc325/src/bin/b.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        wx: [(i64, i64); n],
+    }
+
+    let mut x: Vec<i64> = wx.iter().map(|&(_, xi)| xi).collect();
+    let w: Vec<i64> = wx.iter().map(|&(wi, _)| wi).collect();
+    let mut ans = 0;
+    for _ in 0..24 {
+        let cnt: i64 = x.iter()
+            .zip(&w)
+            .filter(|(&xi, _)| (9..18).contains(&xi))
+            .map(|(_, &wi)| wi)
+            .sum();
+        ans = ans.max(cnt);
+        x.iter_mut().for_each(|xi| {
+            *xi = (*xi + 1) % 24;
+        });
+    }
+
+    println!("{}", ans);
 }
