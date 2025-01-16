0bde40bb8546983fffd2dbfb2a050330eeabea2d
takagi3 <satoshi.takagi@pm.me>
2025-01-17 02:32:42 +0900

    2025/01/17

--- abc326/src/bin/b.rs
+++ abc326/src/bin/b.rs
@@ -1,3 +1,13 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: u32,
+    }
+
+    let ans = (n..=919)
+        .find(|&i| (i / 100) * (i / 10 % 10) == i % 10)
+        .unwrap_or(n);
+
+    println!("{}", ans);
 }
