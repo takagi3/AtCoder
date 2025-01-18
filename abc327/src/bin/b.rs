24bfa338875f0618e473e71a541547633ceea167
takagi3 <satoshi.takagi@pm.me>
2025-01-19 02:30:20 +0900

    2025/01/19

--- abc327/src/bin/b.rs
+++ abc327/src/bin/b.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        b: u64,
+    }
+
+    let mut ans = -1;
+    for i in 1u64.. {
+        match i.checked_pow(i as u32) {
+            Some(power) if power == b => {
+                ans = i as i64;
+                break;
+            }
+            Some(power) if power > b => break,
+            None => break,
+            _ => {}
+        }
+    }
+
+    println!("{}", ans);
 }
