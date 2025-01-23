7f9ad130103faa8504684babef951c60e59c9206
takagi3 <satoshi.takagi@pm.me>
2025-01-24 00:24:16 +0900

    2025/01/24

--- abc328/src/bin/b.rs
+++ abc328/src/bin/b.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        d: [usize; n],
+    }
+
+    let mut ans = 0;
+    for i in 0..n {
+        for j in 0..d[i] {
+            let tmp = format!("{}{}", i + 1, j + 1);
+            let mut ok = true;
+            for k in 0..tmp.len() - 1 {
+                if tmp.chars().nth(k) != tmp.chars().nth(k + 1) {
+                    ok = false;
+                    break;
+                }
+            }
+            if ok {
+                ans += 1;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
