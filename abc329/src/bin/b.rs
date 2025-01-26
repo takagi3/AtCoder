07b213bcb485c121cfc9171531e644b747614fba
takagi3 <satoshi.takagi@pm.me>
2025-01-27 03:29:32 +0900

    2025/01/27

--- abc329/src/bin/b.rs
+++ abc329/src/bin/b.rs
@@ -1,3 +1,13 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    if let Some(&max) = a.iter().max() {
+        let ans = a.iter().filter(|&&x| x != max).max().unwrap_or(&0);
+        println!("{}", ans);
+    }
 }
