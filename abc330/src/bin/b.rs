1c1b4b3a95c0617c8983a6e0a66cfef5b57b1d11
takagi3 <satoshi.takagi@pm.me>
2025-01-29 02:10:16 +0900

    2025/01/29

--- abc330/src/bin/b.rs
+++ abc330/src/bin/b.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        l: usize,
+        r: usize,
+        a: [usize; n],
+    }
+
+    let ans: Vec<usize> = a
+        .iter()
+        .map(|&x| match x {
+            x if x <= l => l,
+            x if x >= r => r,
+            _ => x,
+        })
+        .collect();
+
+    println!(
+        "{}",
+        ans.iter()
+            .map(ToString::to_string)
+            .collect::<Vec<_>>()
+            .join(" ")
+    );
 }
