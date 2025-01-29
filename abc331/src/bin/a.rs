55fa7abd1c407e7e710286093faae1e28c6ddbc7
takagi3 <satoshi.takagi@pm.me>
2025-01-30 02:13:53 +0900

    2025/01/30

--- abc331/src/bin/a.rs
+++ abc331/src/bin/a.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        month: usize,
+        day: usize,
+        mut y: usize,
+        mut m: usize,
+        mut d: usize,
+    }
+
+    d += 1;
+    if d > day {
+        m += 1;
+        d = 1;
+    }
+    if m > month {
+        y += 1;
+        m = 1;
+    }
+
+    println!("{} {} {}", y, m, d);
 }
