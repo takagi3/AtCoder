75e4c1b52a99e6d55cf4e9a87d82337060e95928
takagi3 <satoshi.takagi@pm.me>
2025-01-31 00:27:15 +0900

    2025/01/31

--- abc331/src/bin/b.rs
+++ abc331/src/bin/b.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: u32,
+        s: u32,
+        m: u32,
+        l: u32,
+    }
+
+    let mut ans = u32::MAX;
+    for i in 0..=((n + 5) / 6) {
+        for j in 0..=((n + 7) / 8) {
+            let remaining = n.saturating_sub(i * 6 + j * 8);
+            let k = (remaining + 11) / 12;
+            ans = ans.min(s * i + m * j + l * k);
+        }
+    }
+
+    println!("{}", ans);
 }
