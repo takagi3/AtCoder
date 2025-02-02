5992bedb14496ed8db7b699b1d2083b5002b3a40
takagi3 <satoshi.takagi@pm.me>
2025-02-03 02:17:40 +0900

    2025/02/03

--- abc332/src/bin/b.rs
+++ abc332/src/bin/b.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        k: usize,
+        g: usize,
+        m: usize,
+    }
+
+    let (mut glass, mut mag) = (0, 0);
+    for _ in 0..k {
+        if glass == g {
+            glass = 0;
+        } else if mag == 0 {
+            mag = m;
+        } else {
+            let pour = (g - glass).min(mag);
+            glass += pour;
+            mag -= pour;
+        }
+    }
+
+    println!("{} {}", glass, mag);
 }
