e2eb85cd6afd5bfb7e7308221e65a0d1785d8d7f
takagi3 <satoshi.takagi@pm.me>
2025-02-06 00:11:15 +0900

    2025/02/06

--- abc333/src/bin/b.rs
+++ abc333/src/bin/b.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+use proconio::marker::Chars;
+
+fn circular_distance(pair: &[char]) -> i32 {
+    let diff = (pair[0] as i32 - pair[1] as i32).abs();
+    diff.min(5 - diff)
+}
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+        t: Chars,
+    }
+
+    println!("{}", if circular_distance(&s) == circular_distance(&t) { "Yes" } else { "No" });
 }
