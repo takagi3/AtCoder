7802d7c26ec54e038fec6ea02be4bf41c795926e
takagi3 <satoshi.takagi@pm.me>
2025-02-09 23:56:06 +0900

    2025/02/09

--- abc334/src/bin/b.rs
+++ abc334/src/bin/b.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: i64,
+        m: i64,
+        l: i64,
+        r: i64,
+    }
+
+    let k_min = (l - a + (m - 1)).div_euclid(m);
+    let k_max = (r - a).div_euclid(m);
+    let ans = if k_max >= k_min { k_max - k_min + 1 } else { 0 };
+
+    println!("{}", ans);
 }
