9cc2044faab6621860e27fbdf775c068a51438c9
takagi3 <satoshi.takagi@pm.me>
2025-02-11 22:18:46 +0900

    2025/02/11

--- abc335/src/bin/a.rs
+++ abc335/src/bin/a.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        mut s: Chars,
+    }
+
+    let s_length = s.len();
+    s[s_length - 1] = '4';
+
+    println!(
+        "{}",
+        s.iter()
+            .map(ToString::to_string)
+            .collect::<Vec<_>>()
+            .join("")
+    );
 }
