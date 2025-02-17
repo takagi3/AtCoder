b4853c3ce62d7c340fcde53194978657ae247284
takagi3 <satoshi.takagi@pm.me>
2025-02-18 02:06:36 +0900

    2025/02/18

--- abc337/src/bin/a.rs
+++ abc337/src/bin/a.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+use std::cmp::Ordering;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        xy: [(usize, usize); n],
+    }
+
+    let (t, a): (usize, usize) = xy.iter().fold((0, 0), |(t, a), &(x, y)| (t + x, a + y));
+
+    match t.cmp(&a) {
+        Ordering::Greater => println!("Takahashi"),
+        Ordering::Less => println!("Aoki"),
+        Ordering::Equal => println!("Draw"),
+    }
 }
