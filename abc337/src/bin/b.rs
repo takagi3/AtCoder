a9118dcd500ba3fc60d8ad38c466f64c519115cb
takagi3 <satoshi.takagi@pm.me>
2025-02-19 02:16:01 +0900

    2025/02/19

--- abc337/src/bin/b.rs
+++ abc337/src/bin/b.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let mut expected: char = 'A';
+    for &c in &s {
+        if c < expected {
+            println!("No");
+            return;
+        }
+        if c == 'A' {
+            expected = 'A';
+        } else if c == 'B' {
+            expected = 'B';
+        } else if c == 'C' {
+            expected = 'C';
+        }
+    }
+    println!("Yes");
 }
