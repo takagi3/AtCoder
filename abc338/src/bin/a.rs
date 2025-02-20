e58558baca0a0868f291c65d02eb1b8bf5099ad5
takagi3 <satoshi.takagi@pm.me>
2025-02-21 01:31:07 +0900

    2025/02/21

--- abc338/src/bin/a.rs
+++ abc338/src/bin/a.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let ans = if s.first().map(|&c| c.is_uppercase()).unwrap_or(false)
+        && s.iter().skip(1).all(|&c| c.is_lowercase())
+    {
+        "Yes"
+    } else {
+        "No"
+    };
+
+    println!("{}", ans);
 }
