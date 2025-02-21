cadc2976427b891f2b1c80d5165d0747eb141bf1
takagi3 <satoshi.takagi@pm.me>
2025-02-22 03:15:17 +0900

    2025/02/22

--- abc338/src/bin/b.rs
+++ abc338/src/bin/b.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let mut cnt = vec![0; 26];
+    for c in s {
+        cnt[(c as u8 - b'a') as usize] += 1;
+    }
+
+    let (i, _) = cnt
+        .iter()
+        .enumerate()
+        .rev()
+        .max_by_key(|&(_, &v)| v)
+        .unwrap();
+    println!("{}", (b'a' + i as u8) as char);
 }
