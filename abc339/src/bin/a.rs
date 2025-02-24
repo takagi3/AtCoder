1cf9a7425c6a27a523492e78d8274724a17385d9
takagi3 <satoshi.takagi@pm.me>
2025-02-25 01:20:48 +0900

    2025/02/25

--- abc339/src/bin/a.rs
+++ abc339/src/bin/a.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let mut ans = String::new();
+    for c in s {
+        if c == '.' {
+            ans.clear();
+        } else {
+            ans.push(c);
+        }
+    }
+
+    println!("{}", ans);
 }
