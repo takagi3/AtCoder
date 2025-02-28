8e38f6b532f37f4ea88d3ef8aa49122bdc74ff34
takagi3 <satoshi.takagi@pm.me>
2025-03-01 03:45:00 +0900

    2025/03/01

--- abc340/src/bin/b.rs
+++ abc340/src/bin/b.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        queries: [(usize, usize); n],
+    }
+
+    let mut stack = Vec::with_capacity(n);
+    for (kind, val) in queries {
+        match kind {
+            1 => {
+                stack.push(val);
+            }
+            2 => {
+                println!("{}", stack[stack.len() - val]);
+            }
+            _ => (),
+        }
+    }
 }
