c64bfc237ba1aa8a0625731df7dfea4270d58ea6
takagi3 <satoshi.takagi@pm.me>
2025-03-06 02:32:56 +0900

    2025/03/06

--- abc342/src/bin/a.rs
+++ abc342/src/bin/a.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    if let Some(i) = s.windows(2).position(|w| w[0] != w[1]) {
+        let ans = if i == 0 {
+            if s.get(2).map_or(false, |&c| c == s[1]) {
+                1
+            } else {
+                2
+            }
+        } else {
+            i + 2
+        };
+        println!("{}", ans);
+    }
 }
