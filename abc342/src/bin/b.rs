d3c8c2265575ed1dcc880c03cacc2c5be06261a5
takagi3 <satoshi.takagi@pm.me>
2025-03-07 01:48:32 +0900

    2025/03/07

--- abc342/src/bin/b.rs
+++ abc342/src/bin/b.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+use std::collections::HashMap;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        p: [usize; n],
+        q: usize,
+        ab: [(usize, usize); q],
+    }
+
+    let index = p
+        .into_iter()
+        .enumerate()
+        .map(|(i, x)| (x, i))
+        .collect::<HashMap<_, _>>();
+    ab.into_iter()
+        .for_each(|(a, b)| println!("{}", if index[&a] < index[&b] { a } else { b }));
 }
