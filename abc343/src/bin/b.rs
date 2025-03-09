5dcb9f2a2658d16470582c054694d6f88c291cd6
takagi3 <satoshi.takagi@pm.me>
2025-03-10 01:56:35 +0900

    2025/03/10

--- abc343/src/bin/b.rs
+++ abc343/src/bin/b.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [[i32; n]; n],
+    }
+
+    for row in &a {
+        for (j, &val) in row.iter().enumerate() {
+            if val == 1 {
+                print!("{} ", j + 1);
+            }
+        }
+        println!();
+    }
 }
