218bcc83327142857edec15ab543a823e9a6c3fb
takagi3 <satoshi.takagi@pm.me>
2025-03-18 02:31:53 +0900

    2025/03/18

--- abc346/src/bin/a.rs
+++ abc346/src/bin/a.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let ans: Vec<usize> = a.windows(2).map(|w| w[0] * w[1]).collect();
+
+    println!(
+        "{}",
+        ans.iter()
+            .map(usize::to_string)
+            .collect::<Vec<_>>()
+            .join(" ")
+    );
 }
