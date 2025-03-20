8db22900bd57834730abfd179d0ec48435465528
takagi3 <satoshi.takagi@pm.me>
2025-03-21 02:14:25 +0900

    2025/03/21

--- abc347/src/bin/a.rs
+++ abc347/src/bin/a.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    }
+
+    println!(
+        "{}",
+        a.into_iter()
+            .filter(|&x| x % k == 0)
+            .map(|x| (x / k).to_string())
+            .collect::<Vec<_>>()
+            .join(" ")
+    );
 }
