c5cf329d739d090888b145213ff1fb31cb5d25ce
takagi3 <satoshi.takagi@pm.me>
2025-03-22 13:18:27 +0900

    2025/03/22

--- abc347/src/bin/b.rs
+++ abc347/src/bin/b.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+use std::collections::HashSet;
+
 fn main() {
-    todo!();
+    input! {
+        s: String,
+    }
+
+    let mut substrings: HashSet<&str> = HashSet::new();
+    for i in 0..s.len() {
+        for j in i + 1..s.len() + 1 {
+            substrings.insert(&s[i..j]);
+        }
+    }
+
+    println!("{}", substrings.len());
 }
