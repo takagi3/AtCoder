e4f793b36a41692e7e81e1c0fb84d14e228f1465
takagi3 <satoshi.takagi@pm.me>
2025-03-24 02:07:25 +0900

    2025/03/24

--- abc348/src/bin/a.rs
+++ abc348/src/bin/a.rs
@@ -1,3 +1,13 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+    }
+
+    let ans: String = (1..=n)
+        .map(|i| if i % 3 == 0 { 'x' } else { 'o' })
+        .collect();
+
+    println!("{}", ans);
 }
