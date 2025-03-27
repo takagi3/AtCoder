273cb3041d36a57c2985142e455628fecb7a59e1
takagi3 <satoshi.takagi@pm.me>
2025-03-28 00:48:37 +0900

    2025/03/28

--- abc349/src/bin/b.rs
+++ abc349/src/bin/b.rs
@@ -1,3 +1,26 @@
+use proconio::input;
+use proconio::marker::Chars;
+use std::collections::HashMap;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let mut freq = HashMap::new();
+    for ch in s {
+        *freq.entry(ch).or_insert(0) += 1;
+    }
+    let mut count_map = HashMap::new();
+    for &count in freq.values() {
+        *count_map.entry(count).or_insert(0) += 1;
+    }
+    for &cnt in count_map.values() {
+        if cnt != 2 {
+            println!("No");
+            return;
+        }
+    }
+
+    println!("Yes");
 }
