743fba4fed696da907a19b03826fedf68c413a28
takagi3 <satoshi.takagi@pm.me>
2025-03-31 01:04:19 +0900

    2025/03/31

--- abc350/src/bin/a.rs
+++ abc350/src/bin/a.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let ans = if ('0'..='2').contains(&s[3]) {
+        "Yes"
+    } else if s[3] == '3' && ('0'..='4').contains(&s[4]) {
+        if s[4] == '1' && s[5] == '6' {
+            "No"
+        } else {
+            "Yes"
+        }
+    } else {
+        "No"
+    };
+
+    println!("{}", ans);
 }
