d21269ea65dac1dcfbbf98da0380d5d222260cbc
takagi3 <satoshi.takagi@pm.me>
2025-04-01 03:18:15 +0900

    2025/03/31

--- abc350/src/bin/a.rs
+++ abc350/src/bin/a.rs
@@ -7,7 +7,11 @@ fn main() {
     }
 
     let ans = if ('0'..='2').contains(&s[3]) {
-        "Yes"
+        if s[4] == '0' && s[5] == '0' {
+            "No"
+        } else {
+            "Yes"
+        }
     } else if s[3] == '3' && ('0'..='4').contains(&s[4]) {
         if s[4] == '1' && s[5] == '6' {
             "No"
