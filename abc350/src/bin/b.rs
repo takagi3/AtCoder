a4ab811940fa6f9568dec91854f682ce6cef48c6
takagi3 <satoshi.takagi@pm.me>
2025-04-01 03:14:12 +0900

    2025/04/01

--- abc350/src/bin/b.rs
+++ abc350/src/bin/b.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        q: usize,
+        t: [usize; q],
+    }
+
+    let mut tooth: Vec<bool> = vec![true; n];
+    for x in t {
+        tooth[x - 1] ^= true;
+    }
+
+    println!("{}", tooth.iter().copied().filter(|b| *b).count());
 }
