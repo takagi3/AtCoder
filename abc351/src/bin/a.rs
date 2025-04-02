e9b324b0bf90a98811fd1e185249722e940693d1
takagi3 <satoshi.takagi@pm.me>
2025-04-03 01:30:35 +0900

    2025/04/03

--- abc351/src/bin/a.rs
+++ abc351/src/bin/a.rs
@@ -1,3 +1,10 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: [u32; 9],
+        b: [u32; 8],
+    }
+
+    println!("{}", a.iter().sum::<u32>() - b.iter().sum::<u32>() + 1);
 }
