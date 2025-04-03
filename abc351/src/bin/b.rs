b9d8bdb6f2aab829d137279a60626834bc8da55a
takagi3 <satoshi.takagi@pm.me>
2025-04-04 02:14:15 +0900

    2025/04/04

--- abc351/src/bin/b.rs
+++ abc351/src/bin/b.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [String; n],
+        b: [String; n],
+    }
+
+    for (i, (a_line, b_line)) in a.iter().zip(b.iter()).enumerate() {
+        if let Some(j) = a_line
+            .chars()
+            .zip(b_line.chars())
+            .position(|(ac, bc)| ac != bc)
+        {
+            println!("{} {}", i + 1, j + 1);
+        }
+    }
 }
