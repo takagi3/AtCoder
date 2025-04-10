66100081d2d2e5f76e669a9a7cf9f901b48a2397
takagi3 <satoshi.takagi@pm.me>
2025-04-11 03:20:27 +0900

    2025/04/11

--- abc353/src/bin/b.rs
+++ abc353/src/bin/b.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    }
+
+    let mut ans = 0;
+    let mut iter = a.iter().peekable();
+    let mut car = 0;
+    while let Some(&current) = iter.next() {
+        car += current;
+        if let Some(&&next) = iter.peek() {
+            if car + next > k {
+                ans += 1;
+                car = 0;
+            }
+        }
+    }
+
+    println!("{}", ans + 1);
 }
