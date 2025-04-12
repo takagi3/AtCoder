ce1139e99fb224a114330935c36c24c45ced619d
takagi3 <satoshi.takagi@pm.me>
2025-04-13 04:29:31 +0900

    2025/04/13

--- abc354/src/bin/a.rs
+++ abc354/src/bin/a.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        h: i32,
+    }
+
+    let mut _t = 0;
+    for i in 0.. {
+        _t = (1 << (i + 1)) - 1;
+        if _t > h {
+            println!("{}", i + 1);
+            break;
+        }
+    }
 }
