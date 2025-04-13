442dce597a37227e4d9559f5207c95447f0e5658
takagi3 <satoshi.takagi@pm.me>
2025-04-14 00:30:02 +0900

    2025/04/14

--- abc354/src/bin/b.rs
+++ abc354/src/bin/b.rs
@@ -1,3 +1,13 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut sc: [(String, usize); n],
+    }
+
+    let sum: usize = sc.iter().map(|(_, c)| c).sum();
+    sc.sort();
+
+    println!("{}", sc[sum % n].0);
 }
