84d71dde28694e9aa4a73542acd260a173dbea92
takagi3 <satoshi.takagi@pm.me>
2025-04-16 00:44:30 +0900

    2025/04/16

--- abc355/src/bin/a.rs
+++ abc355/src/bin/a.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: i32,
+        b: i32,
+    }
+
+    match (a, b) {
+        (1, 2) | (2, 1) => println!("3"),
+        (1, 3) | (3, 1) => println!("2"),
+        (2, 3) | (3, 2) => println!("1"),
+        _ => println!("-1"),
+    }
 }
