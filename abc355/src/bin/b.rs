da8694f5c1f72e243f98fb6c6904293b4c9a176c
takagi3 <satoshi.takagi@pm.me>
2025-04-17 01:44:47 +0900

    2025/04/17

--- abc355/src/bin/b.rs
+++ abc355/src/bin/b.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        mut a: [usize; n],
+        b: [usize; m],
+    }
+
+    a.sort();
+    println!(
+        "{}",
+        if n >= 2
+            && a.windows(2)
+                .any(|w| b.iter().all(|&x| x < w[0] || w[1] < x))
+        {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
