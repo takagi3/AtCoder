60594090fb14d0bdc1358b19fc9bfca6fb90188c
takagi3 <satoshi.takagi@pm.me>
2025-04-19 04:28:01 +0900

    2025/04/19

--- abc356/src/bin/a.rs
+++ abc356/src/bin/a.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        l: usize,
+        r: usize,
+    }
+
+    println!(
+        "{}",
+        (1..l)
+            .chain((l..=r).rev())
+            .chain(r + 1..=n)
+            .map(|x| x.to_string())
+            .collect::<Vec<_>>()
+            .join(" ")
+    );
 }
