c7d02c160be183e0e69857b5e73a6dce64a94044
takagi3 <satoshi.takagi@pm.me>
2025-04-20 20:20:46 +0900

    2025/04/20

--- abc356/src/bin/b.rs
+++ abc356/src/bin/b.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        _n: usize,
+        m: usize,
+        a: [usize; m],
+        x: [[usize; m]; _n],
+    }
+
+    println!(
+        "{}",
+        if (0..m).all(|j| x.iter().map(|row| row[j]).sum::<usize>() >= a[j]) {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
