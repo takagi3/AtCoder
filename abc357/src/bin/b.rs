9b4fc2c4dcfe4202146b0ca054d69a746ae71d1f
takagi3 <satoshi.takagi@pm.me>
2025-04-23 02:39:07 +0900

    2025/04/23

--- abc357/src/bin/b.rs
+++ abc357/src/bin/b.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let cnt_upper = s.iter().filter(|c| c.is_uppercase()).count();
+    let ans: String = if cnt_upper > s.len() - cnt_upper {
+        s.iter().map(|&c| c.to_ascii_uppercase()).collect()
+    } else {
+        s.iter().map(|&c| c.to_ascii_lowercase()).collect()
+    };
+
+    println!("{}", ans);
 }
