4889472483b30c385c84cdc335b2053b62d64a7a
takagi3 <satoshi.takagi@pm.me>
2025-04-28 00:03:09 +0900

    2025/04/28

--- abc358/src/bin/b.rs
+++ abc358/src/bin/b.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: i64,
+        t: [i64; n],
+    }
+
+    t.into_iter()
+        .scan(0_i64, |now, task| {
+            *now = (*now).max(task) + a;
+            Some(*now)
+        })
+        .for_each(|time| println!("{}", time));
 }
