282011de588d5954009da6642b74641e9c6511c3
takagi3 <satoshi.takagi@pm.me>
2025-05-01 00:24:40 +0900

    2025/05/01

--- abc359/src/bin/b.rs
+++ abc359/src/bin/b.rs
@@ -1,3 +1,10 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [i64; 2 * n],
+    }
+
+    println!("{}", a.windows(3).filter(|w| w[0] == w[2]).count());
 }
