01d50ecafcaeac0fc68f474758bb663bce03e005
takagi3 <satoshi.takagi@pm.me>
2025-05-03 04:08:46 +0900

    2025/05/03

--- abc360/src/bin/a.rs
+++ abc360/src/bin/a.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    println!(
+        "{}",
+        if s[0] == 'R' && (s[1] == 'M' || s[2] == 'M') || s[1] == 'R' && s[2] == 'M' {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
