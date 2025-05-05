9964488d9646f2f811207ae421af9b9c6ff2a880
takagi3 <satoshi.takagi@pm.me>
2025-05-05 23:49:13 +0900

    2025/05/05

--- abc360/src/bin/b.rs
+++ abc360/src/bin/b.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        s: String,
+        t: String,
+    }
+
+    if s.len() == 1 && s == t {
+        println!("Yes");
+        return;
+    }
+    let s: Vec<char> = s.chars().collect();
+    let s_len = s.len();
+    let t = t.as_str();
+    for i in 1..s_len {
+        for k in 1..=i {
+            let tmp: String = (1..)
+                .map(|j| i * j - k)
+                .take_while(|&idx| idx < s_len)
+                .map(|idx| s[idx])
+                .collect();
+
+            if tmp == t {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+
+    println!("No");
 }
