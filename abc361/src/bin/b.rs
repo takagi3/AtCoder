2d794b841e7de3c0786213650344539777e062a7
takagi3 <satoshi.takagi@pm.me>
2025-05-08 01:32:06 +0900

    2025/05/08

--- abc361/src/bin/b.rs
+++ abc361/src/bin/b.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        (a, b, c, d, e, f, g, h, i, j, k, l): (i32, i32, i32, i32, i32, i32, i32, i32, i32, i32, i32, i32)
+    }
+
+    println!(
+        "{}",
+        if j <= a || k <= b || l <= c || d <= g || e <= h || f <= i {
+            "No"
+        } else {
+            "Yes"
+        }
+    );
 }
