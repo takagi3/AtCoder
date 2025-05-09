bbc721e3c09b43093298890e6f2257dd3805de1d
takagi3 <satoshi.takagi@pm.me>
2025-05-10 03:53:45 +0900

    2025/05/10

--- abc362/src/bin/a.rs
+++ abc362/src/bin/a.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        r: u32,
+        g: u32,
+        b: u32,
+        c: String,
+    }
+
+    println!(
+        "{}",
+        [("Red", r), ("Green", g), ("Blue", b)]
+            .iter()
+            .filter(|&&(name, _)| name != c)
+            .map(|&(_, value)| value)
+            .min()
+            .unwrap()
+    );
 }
