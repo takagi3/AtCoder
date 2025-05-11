22666c0d61cf6cc13b15ae0dcae0d761ea5945fe
takagi3 <satoshi.takagi@pm.me>
2025-05-11 16:11:04 +0900

    2025/05/11

--- abc362/src/bin/b.rs
+++ abc362/src/bin/b.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        (xa, ya): (i64, i64),
+        (xb, yb): (i64, i64),
+        (xc, yc): (i64, i64),
+    }
+
+    let mut d = [
+        (xa - xb).pow(2) + (ya - yb).pow(2),
+        (xa - xc).pow(2) + (ya - yc).pow(2),
+        (xb - xc).pow(2) + (yb - yc).pow(2),
+    ];
+    d.sort_unstable();
+
+    println!("{}", if d[0] + d[1] == d[2] { "Yes" } else { "No" });
 }
