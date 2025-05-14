dc6f54b56328e71cde46669ea9268427116f256d
takagi3 <satoshi.takagi@pm.me>
2025-05-15 02:14:33 +0900

    2025/05/15

--- abc363/src/bin/b.rs
+++ abc363/src/bin/b.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+use std::cmp::Reverse;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        t: i32,
+        p: usize,
+        mut l: [i32; n],
+    }
+
+    l.sort_unstable_by_key(|&d| Reverse(d - t));
+
+    println!("{}", (l[p - 1] - t).clamp(i32::MIN, 0).abs());
 }
