9f615566b767e64a60977d9408acfaf2687fb7ec
takagi3 <satoshi.takagi@pm.me>
2025-05-19 02:14:02 +0900

    2025/05/19

--- abc364/src/bin/a.rs
+++ abc364/src/bin/a.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: [String; n],
+    }
+
+    println!(
+        "{}",
+        if s[..n - 1]
+            .windows(2)
+            .any(|w| w[0] == "sweet" && w[1] == "sweet")
+        {
+            "No"
+        } else {
+            "Yes"
+        }
+    );
 }
