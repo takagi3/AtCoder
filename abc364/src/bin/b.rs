bdc2caf31bf133c6b73ae7207647ec323d9a615a
takagi3 <satoshi.takagi@pm.me>
2025-05-20 01:42:01 +0900

    2025/05/20

--- abc364/src/bin/b.rs
+++ abc364/src/bin/b.rs
@@ -1,3 +1,29 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        h: usize,
+        w: usize,
+        si: usize,
+        sj: usize,
+        grid: [Chars; h],
+        moves: Chars,
+    }
+
+    let (y, x) = moves.into_iter().fold((si - 1, sj - 1), |(y, x), m| {
+        let (ny, nx) = match m {
+            'L' => (y, x.saturating_sub(1)),
+            'R' => (y, (x + 1).min(w - 1)),
+            'U' => (y.saturating_sub(1), x),
+            'D' => ((y + 1).min(h - 1), x),
+            _ => (y, x),
+        };
+        if grid[ny][nx] == '.' {
+            (ny, nx)
+        } else {
+            (y, x)
+        }
+    });
+
+    println!("{} {}", y + 1, x + 1);
 }
