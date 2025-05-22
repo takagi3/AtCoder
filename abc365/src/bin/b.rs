73422d18654a3a290142f811da1e8ad7c63d2e05
takagi3 <satoshi.takagi@pm.me>
2025-05-23 02:17:08 +0900

    2025/05/23

--- abc365/src/bin/b.rs
+++ abc365/src/bin/b.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [u32; n],
+    }
+
+    let &max = a.iter().max().unwrap();
+
+    println!(
+        "{}",
+        a.iter()
+            .enumerate()
+            .filter(|&(_, &v)| v != max)
+            .max_by_key(|&(_, &v)| v)
+            .map(|(i, _)| i + 1)
+            .unwrap_or(0)
+    );
 }
