bd818f80eab58e60533438fe48c4d330174f083d
takagi3 <satoshi.takagi@pm.me>
2025-05-26 01:11:23 +0900

    2025/05/26

--- abc366/src/bin/b.rs
+++ abc366/src/bin/b.rs
@@ -1,3 +1,20 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: [Chars; n],
+    }
+
+    let m = s.iter().map(Vec::len).max().unwrap();
+    let rev = s.iter().rev().collect::<Vec<_>>();
+    for j in 0..m {
+        let row = rev
+            .iter()
+            .map(|v| v.get(j).cloned().unwrap_or('*'))
+            .collect::<Vec<_>>();
+        let last = row.iter().rposition(|&c| c != '*').unwrap();
+        let line = row[..=last].iter().collect::<String>();
+        println!("{}", line);
+    }
 }
