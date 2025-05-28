eff126add174f4730b5b7014275f3350525069d7
takagi3 <satoshi.takagi@pm.me>
2025-05-29 02:20:07 +0900

    2025/05/29

--- abc367/src/bin/a.rs
+++ abc367/src/bin/a.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: u32,
+        b: u32,
+        c: u32,
+    }
+
+    println!(
+        "{}",
+        if (a + 24 - b) % 24 < (c + 24 - b) % 24 {
+            "No"
+        } else {
+            "Yes"
+        }
+    );
 }
