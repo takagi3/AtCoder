c9cf73fea2c76cb19a271336eaa4d86c2752c629
takagi3 <satoshi.takagi@pm.me>
2025-06-02 03:10:08 +0900

    2025/06/02

--- abc368/src/bin/a.rs
+++ abc368/src/bin/a.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        mut a: [usize; n],
+    }
+
+    a.rotate_right(k);
+
+    println!(
+        "{}",
+        a.iter()
+            .map(ToString::to_string)
+            .collect::<Vec<_>>()
+            .join(" ")
+    );
 }
