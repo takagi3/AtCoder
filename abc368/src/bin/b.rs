61cb6867c3461786c7b4589d644b8114b071dfc5
takagi3 <satoshi.takagi@pm.me>
2025-06-03 01:46:57 +0900

    2025/06/03

--- abc368/src/bin/b.rs
+++ abc368/src/bin/b.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+use std::collections::BinaryHeap;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [i32; n],
+    }
+
+    let mut heap = a.into_iter().filter(|&x| x > 0).collect::<BinaryHeap<_>>();
+    let mut ans = 0;
+    while heap.len() > 1 {
+        let mut x = heap.pop().unwrap();
+        let mut y = heap.pop().unwrap();
+        x -= 1;
+        y -= 1;
+        if x > 0 {
+            heap.push(x);
+        }
+        if y > 0 {
+            heap.push(y);
+        }
+        ans += 1;
+    }
+
+    println!("{}", ans);
 }
