35cebb9ddb8f4c34db5278b288a8cbbbaf23c70b
takagi3 <satoshi.takagi@pm.me>
2025-06-05 02:02:26 +0900

    cargo compete new abc369

--- abc369/src/bin/a.rs
+++ abc369/src/bin/a.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: u32,
+        b: u32,
+    }
+
+    println!(
+        "{}",
+        if a == b {
+            1
+        } else if (a + b) % 2 != 0 {
+            2
+        } else {
+            3
+        }
+    );
 }
