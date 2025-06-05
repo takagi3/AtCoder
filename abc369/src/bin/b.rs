d664b16d1e8806821ab6f86f63a49d231594a80c
takagi3 <satoshi.takagi@pm.me>
2025-06-06 01:10:48 +0900

    2025/06/06

--- abc369/src/bin/b.rs
+++ abc369/src/bin/b.rs
@@ -1,3 +1,31 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a_s: [(i32, char); n],
+    }
+
+    let mut ans = 0;
+    let mut last_l: Option<i32> = None;
+    let mut last_r: Option<i32> = None;
+    for &(pos, dir) in &a_s {
+        match dir {
+            'L' => {
+                if let Some(prev) = last_l {
+                    ans += (prev - pos).abs();
+                }
+                last_l = Some(pos);
+            }
+            'R' => {
+                if let Some(prev) = last_r {
+                    ans += (prev - pos).abs();
+                }
+                last_r = Some(pos);
+            }
+            _ => {}
+        }
+    }
+
+    println!("{}", ans);
 }
