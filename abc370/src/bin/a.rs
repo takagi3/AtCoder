5f74d8bdee173060fe2b0683061371a045e799e2
takagi3 <satoshi.takagi@pm.me>
2025-06-08 04:06:19 +0900

    2025/06/08

--- abc370/src/bin/a.rs
+++ abc370/src/bin/a.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        (l, r): (u32, u32)
+    }
+
+    let ans = match (l, r) {
+        (1, 0) => "Yes",
+        (0, 1) => "No",
+        _ => "Invalid",
+    };
+
+    println!("{}", ans);
 }
