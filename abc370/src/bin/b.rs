268037392f566590b2bed533250bd2d69e4cedb5
takagi3 <satoshi.takagi@pm.me>
2025-06-09 01:28:54 +0900

    2025/06/09

--- abc370/src/bin/b.rs
+++ abc370/src/bin/b.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n * (n + 1) / 2],
+    }
+
+    println!(
+        "{}",
+        (1..=n).fold(1, |next, i| {
+            let (r, c) = (next.max(i) - 1, next.min(i) - 1);
+            a[r * (r + 1) / 2 + c]
+        })
+    );
 }
