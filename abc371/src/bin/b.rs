482889ae98b9da6e05c66a2ab5933c2661a5970e
takagi3 <satoshi.takagi@pm.me>
2025-06-23 03:48:30 +0900

    2025/06/23

--- abc371/src/bin/b.rs
+++ abc371/src/bin/b.rs
@@ -1,3 +1,19 @@
+use proconio::{input, marker::Usize1};
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        queries: [(Usize1, char); m],
+    }
+
+    let mut available = vec![true; n];
+    for (idx, c) in queries {
+        if available[idx] && c == 'M' {
+            available[idx] = false;
+            println!("Yes");
+        } else {
+            println!("No");
+        }
+    }
 }
