12dc0e99439bf4c72730cfc8de491ac7ffec6fcf
takagi3 <satoshi.takagi@pm.me>
2025-06-26 02:10:49 +0900

    2025/06/26

--- abc372/src/bin/b.rs
+++ abc372/src/bin/b.rs
@@ -1,3 +1,30 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! { mut m: i32 }
+    
+    const POWERS: [(i32, usize); 11] = [
+        (3_i32.pow(10), 10), (3_i32.pow(9), 9),  (3_i32.pow(8), 8),
+        (3_i32.pow(7), 7),  (3_i32.pow(6), 6),  (3_i32.pow(5), 5),
+        (3_i32.pow(4), 4),  (3_i32.pow(3), 3),  (3_i32.pow(2), 2),
+        (3_i32.pow(1), 1),  (3_i32.pow(0), 0),
+    ];
+
+    let mut result = Vec::new();
+    for &(value, exp) in &POWERS {
+        while m >= value {
+            m -= value;
+            result.push(exp);
+        }
+        if m == 0 {
+            break;
+        }
+    }
+
+    println!("{}", result.len());
+    println!("{}", result.iter()
+        .map(|e| e.to_string())
+        .collect::<Vec<_>>()
+        .join(" ")
+    );
 }
