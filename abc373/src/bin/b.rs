6746813b39cf92ab245c0df047b4dcfc2efc1c1c
takagi3 <satoshi.takagi@pm.me>
2025-06-30 02:22:37 +0900

    2025/06/30

--- abc373/src/bin/b.rs
+++ abc373/src/bin/b.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        s: String,
+    }
+
+    let bytes = s.as_bytes();
+    let positions: Vec<usize> = (b'A'..=b'Z')
+        .map(|c| bytes.iter().position(|&b| b == c).unwrap())
+        .collect();
+
+    println!(
+        "{}",
+        positions
+            .windows(2)
+            .map(|w| w[0].abs_diff(w[1]))
+            .sum::<usize>()
+    );
 }
