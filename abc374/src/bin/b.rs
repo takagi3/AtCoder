6931183d0f5470a0dd727fac409959e104d4d5a2
takagi3 <satoshi.takagi@pm.me>
2025-07-03 02:34:57 +0900

    2025/07/03

--- abc374/src/bin/b.rs
+++ abc374/src/bin/b.rs
@@ -1,3 +1,17 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+        t: Chars,
+    }
+
+    println!(
+        "{}",
+        match s.iter().zip(&t).position(|(a, b)| a != b) {
+            Some(i) => i + 1,
+            None if s.len() == t.len() => 0,
+            None => s.len().min(t.len()) + 1,
+        }
+    );
 }
