608e3e6ceef6e70917f965fbda559b1ae311ba5f
takagi3 <satoshi.takagi@pm.me>
2025-07-06 21:30:51 +0900

    2025/07/06

--- abc375/src/bin/b.rs
+++ abc375/src/bin/b.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        points: [(f64, f64); n],
+    }
+
+    let mut path = Vec::with_capacity(n + 2);
+    path.push((0.0, 0.0));
+    path.extend(points);
+    path.push((0.0, 0.0));
+
+    println!(
+        "{:.12}",
+        path.windows(2)
+            .map(|w| {
+                let (x1, y1) = w[0];
+                let (x2, y2) = w[1];
+                ((x1 - x2).powi(2) + (y1 - y2).powi(2)).sqrt()
+            })
+            .sum::<f64>()
+    );
 }
