b06f8e20716aeb9eed98c4fe55f3e07b0082bda2
takagi3 <satoshi.takagi@pm.me>
2025-07-08 00:29:11 +0900

    2025/07/08

--- abc376/src/bin/a.rs
+++ abc376/src/bin/a.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        c: u32,
+        t: [u32; n],
+    }
+
+    println!(
+        "{}",
+        t.iter()
+            .skip(1)
+            .fold((1usize, t[0]), |(count, last), &time| {
+                if time - last >= c {
+                    (count + 1, time)
+                } else {
+                    (count, last)
+                }
+            })
+            .0
+    );
 }
