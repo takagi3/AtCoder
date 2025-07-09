8b083c8713ba9535960632999c5bf5b6d1aa083b
takagi3 <satoshi.takagi@pm.me>
2025-07-10 00:06:13 +0900

    2025/07/10

--- abc377/src/bin/a.rs
+++ abc377/src/bin/a.rs
@@ -1,3 +1,12 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let abc = ['A', 'B', 'C'].iter().all(|&c| s.contains(&c));
+
+    println!("{}", if abc { "Yes" } else { "No" });
 }
