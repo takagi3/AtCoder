518e4d93da27d21f966275653b0362df1e64ee0a
takagi3 <satoshi.takagi@pm.me>
2025-07-11 02:48:12 +0900

    2025/07/11

--- abc377/src/bin/b.rs
+++ abc377/src/bin/b.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+use proconio::marker::Chars;
+
+const N: usize = 8;
+
 fn main() {
-    todo!();
+    input! {
+        grid: [Chars; N],
+    }
+
+    let free_rows = grid.iter().filter(|row| !row.contains(&'#')).count();
+
+    let free_cols = (0..N)
+        .filter(|&i| grid.iter().all(|row| row[i] != '#'))
+        .count();
+
+    println!("{}", free_rows * free_cols);
 }
