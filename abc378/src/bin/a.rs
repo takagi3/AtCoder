15e70edd055521cc2267254f354160b4eb79db19
takagi3 <satoshi.takagi@pm.me>
2025-07-14 01:47:43 +0900

    2025/07/14

--- abc378/src/bin/a.rs
+++ abc378/src/bin/a.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: [usize; 4],
+    }
+
+    let ball = a.iter().fold([0u32; 5], |mut cnt, &x| {
+        cnt[x] += 1;
+        cnt
+    });
+
+    println!(
+        "{}",
+        ball.iter()
+            .map(|&c| match c {
+                4 => 2,
+                2..=3 => 1,
+                _ => 0,
+            })
+            .sum::<u32>()
+    );
 }
