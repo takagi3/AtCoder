8e221fdb51d5ad0a54247e563f0e6e02083f6082
takagi3 <satoshi.takagi@pm.me>
2025-07-15 00:55:27 +0900

    2025/07/15

--- abc378/src/bin/b.rs
+++ abc378/src/bin/b.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        qr: [(i64, i64); n],
+        query: usize,
+        td: [(usize, i64); query],
+    }
+
+    for (t, d) in td {
+        let (q, r) = qr[t - 1];
+        println!("{}", d + (r + q - d % q) % q);
+    }
 }
