878f2b1f0be66be5f12937dfd70c9ffb8c8954c6
takagi3 <satoshi.takagi@pm.me>
2025-07-17 01:08:55 +0900

    2025/07/17

--- abc379/src/bin/a.rs
+++ abc379/src/bin/a.rs
@@ -1,3 +1,12 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        n: Chars,
+    }
+
+    let [a, b, c] = [n[0], n[1], n[2]];
+
+    println!("{}{}{} {}{}{}", b, c, a, c, a, b);
 }
