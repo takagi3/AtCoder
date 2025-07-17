fee8937dd82b4f9659b271cc311c5a329f1762c5
takagi3 <satoshi.takagi@pm.me>
2025-07-18 01:54:28 +0900

    2025/07/18

--- abc379/src/bin/b.rs
+++ abc379/src/bin/b.rs
@@ -1,3 +1,16 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        _n: usize,
+        k: usize,
+        s: Chars,
+    }
+
+    let ans = s
+        .split(|&c| c == 'X')
+        .map(|seg| seg.len() / k)
+        .sum::<usize>();
+
+    println!("{}", ans);
 }
