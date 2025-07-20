3a8a4331eba7b94f0aa049e76b32559ad6e3c2c3
takagi3 <satoshi.takagi@pm.me>
2025-07-20 23:35:18 +0900

    2025/07/20

--- abc380/src/bin/a.rs
+++ abc380/src/bin/a.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        n: Chars,
+    }
+
+    let counts = n.iter().fold([0_u32; 3], |mut acc, &c| {
+        match c {
+            '1' => acc[0] += 1,
+            '2' => acc[1] += 1,
+            '3' => acc[2] += 1,
+            _ => {}
+        }
+        acc
+    });
+
+    println!("{}", if counts == [1, 2, 3] { "Yes" } else { "No" });
 }
