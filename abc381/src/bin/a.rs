42f89b6e42ad47f85b721c0528ac29496bea3cf6
takagi3 <satoshi.takagi@pm.me>
2025-07-23 01:35:29 +0900

    2025/07/23

--- abc381/src/bin/a.rs
+++ abc381/src/bin/a.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: Chars,
+    }
+
+    let ok = if n % 2 == 1 {
+        let half = n / 2;
+        s[..half].iter().all(|&c| c == '1')
+            && s[half] == '/'
+            && s[half + 1..].iter().all(|&c| c == '2')
+    } else {
+        false
+    };
+
+    println!("{}", if ok { "Yes" } else { "No" });
 }
