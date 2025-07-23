253c1510564a2f6730aeea3705e7e2a47e23d289
takagi3 <satoshi.takagi@pm.me>
2025-07-24 01:19:38 +0900

    2025/07/24

--- abc381/src/bin/b.rs
+++ abc381/src/bin/b.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+use proconio::marker::Chars;
+use std::collections::HashSet;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    if s.len() % 2 != 0 {
+        println!("No");
+        return;
+    }
+    let mut seen = HashSet::new();
+    let ok = s
+        .chunks(2)
+        .all(|chunk| chunk[0] == chunk[1] && seen.insert(chunk[0]));
+
+    println!("{}", if ok { "Yes" } else { "No" });
 }
