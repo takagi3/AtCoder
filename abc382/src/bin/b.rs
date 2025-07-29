2487885c92f017da953753e7510fd8572ab45c97
takagi3 <satoshi.takagi@pm.me>
2025-07-30 02:20:27 +0900

    2025/07/30

--- abc382/src/bin/b.rs
+++ abc382/src/bin/b.rs
@@ -1,3 +1,17 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        _n: usize,
+        d: usize,
+        mut s: Chars,
+    }
+
+    s.iter_mut()
+        .rev()
+        .filter(|c| **c == '@')
+        .take(d)
+        .for_each(|c| *c = '.');
+
+    println!("{}", s.into_iter().collect::<String>());
 }
