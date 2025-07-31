0dd53e8d03a6cd5ad8d05b1fb362a52e79b524e0
takagi3 <satoshi.takagi@pm.me>
2025-08-01 01:34:37 +0900

    2025/08/01

--- abc383/src/bin/a.rs
+++ abc383/src/bin/a.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        tv: [(u32, u32); n],
+    }
+
+    println!(
+        "{}",
+        tv.windows(2).fold(tv[0].1, |acc, pair| {
+            let dt = pair[1].0 - pair[0].0;
+            acc.saturating_sub(dt) + pair[1].1
+        })
+    );
 }
