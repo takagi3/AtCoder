48d9ccf1b3df71f88cae6b86a637eabd973f06f3
takagi3 <satoshi.takagi@pm.me>
2025-08-07 00:44:10 +0900

    2025/08/07

--- abc384/src/bin/b.rs
+++ abc384/src/bin/b.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut r: i64,
+        da: [(i32, i64); n],
+    }
+
+    for (d, a) in da {
+        if (d == 1 && (1600..=2799).contains(&r)) || (d == 2 && (1200..=2399).contains(&r)) {
+            r += a;
+        }
+    }
+
+    println!("{}", r);
 }
