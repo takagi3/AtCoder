6cc0213a63b1faae9c5adad6dd3bb34a1f1f5712
takagi3 <satoshi.takagi@pm.me>
2025-08-09 03:30:55 +0900

    2025/08/09

--- abc385/src/bin/a.rs
+++ abc385/src/bin/a.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        (a, b, c): (u32, u32, u32),
+    }
+
+    println!(
+        "{}",
+        if a == b && b == c || a + b == c || a + c == b || b + c == a {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
