e616cfc1f1527e8003a45118d31408c8a1164562
takagi3 <satoshi.takagi@pm.me>
2025-08-14 02:16:00 +0900

    2025/08/14

--- abc386/src/bin/a.rs
+++ abc386/src/bin/a.rs
@@ -1,3 +1,12 @@
+use proconio::input;
+use std::collections::HashSet;
+
 fn main() {
-    todo!();
+    input! {
+        cards: [u32; 4],
+    }
+
+    let kind: HashSet<u32> = cards.into_iter().collect();
+
+    println!("{}", if kind.len() == 2 { "Yes" } else { "No" });
 }
