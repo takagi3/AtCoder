a9e6dd85fead21fc9ddb66946f6b2352970bacd4
takagi3 <satoshi.takagi@pm.me>
2025-08-15 02:13:06 +0900

    2025/08/15

--- abc386/src/bin/b.rs
+++ abc386/src/bin/b.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    let mut ans = 0;
+    let mut i = 0;
+    while i < s.len() {
+        ans += 1;
+        i += if s[i] == '0' && i + 1 < s.len() && s[i + 1] == '0' {
+            2
+        } else {
+            1
+        };
+    }
+
+    println!("{}", ans);
 }
