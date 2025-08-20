1e6326f3f0b5941c14aefea909b51a32e42fde81
takagi3 <satoshi.takagi@pm.me>
2025-08-21 00:05:42 +0900

    2025/08/21

--- abc387/src/bin/b.rs
+++ abc387/src/bin/b.rs
@@ -1,3 +1,15 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        x: u32,
+    }
+
+    const TOTAL: u32 = 2025;
+    let count = (1..=9)
+        .flat_map(|i| (1..=9).map(move |j| i * j))
+        .filter(|&v| v == x)
+        .count() as u32;
+
+    println!("{}", TOTAL - x * count);
 }
