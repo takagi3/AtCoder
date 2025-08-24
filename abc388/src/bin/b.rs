2fd159bbf163eef826c0345afa3e8ded9242cc9b
takagi3 <satoshi.takagi@pm.me>
2025-08-25 02:17:24 +0900

    2025/08/25

--- abc388/src/bin/b.rs
+++ abc388/src/bin/b.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        d: usize,
+        tl: [(usize, usize); n],
+    }
+
+    let base: Vec<usize> = tl.iter().map(|&(t, l)| t * l).collect();
+    let ts: Vec<usize> = tl.iter().map(|&(t, _)| t).collect();
+
+    for k in 1..=d {
+        println!(
+            "{}",
+            base.iter().zip(&ts).map(|(b, &t)| b + t * k).max().unwrap()
+        );
+    }
 }
