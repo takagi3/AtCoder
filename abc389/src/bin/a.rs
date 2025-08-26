f8c3a01cae5c51572dc77cade2fdaf72b4dec4fb
takagi3 <satoshi.takagi@pm.me>
2025-08-27 00:38:36 +0900

    2025/08/27

--- abc389/src/bin/a.rs
+++ abc389/src/bin/a.rs
@@ -1,3 +1,10 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    println!("{}", (s[0] as u8 - '0' as u8) * (s[2] as u8 - '0' as u8));
 }
