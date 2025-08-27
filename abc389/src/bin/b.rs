062ee08e12ac11b4b9a40f0ddfdbfe26228d79dd
takagi3 <satoshi.takagi@pm.me>
2025-08-28 00:22:58 +0900

    2025/08/28

--- abc389/src/bin/b.rs
+++ abc389/src/bin/b.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        x: u64,
+    }
+
+    let ans = (1..=20)
+        .scan(1u64, |fact, i| {
+            *fact *= i;
+            Some((*fact, i))
+        })
+        .find(|&(fact, _)| fact == x)
+        .map_or(0, |(_, i)| i);
+
+    println!("{}", ans);
 }
