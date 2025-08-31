4d77d34af128c937f0a770d50afe4ce807a11569
takagi3 <satoshi.takagi@pm.me>
2025-09-01 01:31:13 +0900

    2025/08/31

--- abc390/src/bin/a.rs
+++ abc390/src/bin/a.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        mut a: [i32; 5],
+    }
+
+    let mut cnt: u32 = 0;
+    for i in 0..4 {
+        if a[i] > a[i + 1] {
+            (a[i], a[i + 1]) = (a[i + 1], a[i]);
+            cnt += 1;
+        }
+    }
+
+    println!("{}", if cnt == 1 { "Yes" } else { "No" });
 }
