0a84ef6f3142d4a661975a78988301ac183dcd87
takagi3 <satoshi.takagi@pm.me>
2025-09-01 01:46:12 +0900

    2025/09/01

--- abc390/src/bin/b.rs
+++ abc390/src/bin/b.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [i64; n],
+    }
+
+    let ok = if n <= 2 {
+        true
+    } else {
+        a.windows(3).all(|w| {
+            let (x, y, z) = (w[0] as i128, w[1] as i128, w[2] as i128);
+            x * z == y * y
+        })
+    };
+
+    println!("{}", if ok { "Yes" } else { "No" });
 }
