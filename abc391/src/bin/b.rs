48f5664c9f448dfec6807b15bc2f3f8bebf5c82d
takagi3 <satoshi.takagi@pm.me>
2025-09-04 00:53:52 +0900

    2025/09/04

--- abc391/src/bin/b.rs
+++ abc391/src/bin/b.rs
@@ -1,3 +1,20 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        s: [Chars; n],
+        t: [Chars; m],
+    }
+
+    for i in 0..=n - m {
+        for j in 0..=n - m {
+            let ok = (0..m).all(|k| &s[i + k][j..j + m] == &t[k][..]);
+            if ok {
+                println!("{} {}", i + 1, j + 1);
+                return;
+            }
+        }
+    }
 }
