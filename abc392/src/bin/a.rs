bac012b5e390490fca2e269518a9386896de5c80
takagi3 <satoshi.takagi@pm.me>
2025-09-06 00:52:10 +0900

    2025/09/06

--- abc392/src/bin/a.rs
+++ abc392/src/bin/a.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: [u32; 3],
+    }
+
+    println!(
+        "{}",
+        if a[0] * a[1] == a[2] || a[0] * a[2] == a[1] || a[1] * a[2] == a[0] {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
