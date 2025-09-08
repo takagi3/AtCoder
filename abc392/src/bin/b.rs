31889385e5aae077bbed99bf1578d4e336426016
takagi3 <satoshi.takagi@pm.me>
2025-09-09 01:31:27 +0900

    2025/09/09

--- abc392/src/bin/b.rs
+++ abc392/src/bin/b.rs
@@ -1,3 +1,17 @@
+use itertools::Itertools;
+use proconio::input;
+use std::collections::HashSet;
+
 fn main() {
-    todo!();
+    input! {
+        n: u32,
+        m: usize,
+        a: [u32; m],
+    }
+
+    let set: HashSet<_> = a.into_iter().collect();
+    let ans: Vec<_> = (1..=n).filter(|x| !set.contains(x)).collect();
+
+    println!("{}", ans.len());
+    println!("{}", ans.iter().join(" "));
 }
