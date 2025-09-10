7c543bb5afc9031320ee3d142d040b0d9b3a97a6
takagi3 <satoshi.takagi@pm.me>
2025-09-11 01:19:46 +0900

    2025/09/11

--- abc393/src/bin/a.rs
+++ abc393/src/bin/a.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        (s1, s2): (String, String),
+    }
+
+    println!(
+        "{}",
+        match (s1.as_str(), s2.as_str()) {
+            ("sick", "sick") => 1,
+            ("sick", "fine") => 2,
+            ("fine", "sick") => 3,
+            ("fine", "fine") => 4,
+            _ => 0,
+        }
+    );
 }
