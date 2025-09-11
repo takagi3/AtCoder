b55b49e42fd297475119b30156fef68b09c1b21a
takagi3 <satoshi.takagi@pm.me>
2025-09-12 00:37:19 +0900

    2025/09/12

--- abc393/src/bin/b.rs
+++ abc393/src/bin/b.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        s: String,
+    }
+
+    let b = s.as_bytes();
+    let n = b.len();
+    let mut ans = 0usize;
+    for i in 0..n {
+        let max_j = (n.saturating_sub(i) - 1) / 2;
+        for j in 1..=max_j {
+            if b[i] == b'A' && b[i + j] == b'B' && b[i + 2 * j] == b'C' {
+                ans += 1;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
