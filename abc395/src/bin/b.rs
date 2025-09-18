5fe90d4ed48f2b8ba6a8cd588f43d8aa95649204
takagi3 <satoshi.takagi@pm.me>
2025-09-19 01:57:35 +0900

    2025/09/19

--- abc395/src/bin/b.rs
+++ abc395/src/bin/b.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize
+    }
+
+    for r in 0..n {
+        let mut row = String::with_capacity(n);
+        for c in 0..n {
+            let d = r.min(c).min(n - 1 - r).min(n - 1 - c);
+            row.push(if d % 2 == 0 { '#' } else { '.' });
+        }
+        println!("{}", row);
+    }
 }
