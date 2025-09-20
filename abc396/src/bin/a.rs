eb0ab7c067e754e8ec4c73e2a2259050a4f06fb6
takagi3 <satoshi.takagi@pm.me>
2025-09-21 01:36:00 +0900

    2025/09/21

--- abc396/src/bin/a.rs
+++ abc396/src/bin/a.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    println!(
+        "{}",
+        if a.windows(3).any(|w| w[0] == w[1] && w[1] == w[2]) {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
