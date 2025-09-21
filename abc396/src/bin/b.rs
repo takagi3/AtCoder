a68d4e3adb662f42fd6476979f6509e29d06fb0f
takagi3 <satoshi.takagi@pm.me>
2025-09-22 00:55:03 +0900

    2025/09/22

--- abc396/src/bin/b.rs
+++ abc396/src/bin/b.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        q: usize
+    }
+
+    let mut cards = vec![0; 100];
+    for _ in 0..q {
+        input! { t: i32 }
+        match t {
+            1 => {
+                input! { x: i32 }
+                cards.push(x);
+            }
+            2 => println!("{}", cards.pop().unwrap()),
+            _ => unreachable!(),
+        }
+    }
 }
