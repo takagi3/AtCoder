8068830677ba5246c7540c18db459f953e682280
takagi3 <satoshi.takagi@pm.me>
2025-09-25 00:47:01 +0900

    2025/09/25

--- abc397/src/bin/b.rs
+++ abc397/src/bin/b.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+use proconio::marker::Chars;
+
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+
+    println!(
+        "{}",
+        s.windows(2).filter(|w| w[0] == w[1]).count()
+            + ((s[0] == 'o') as usize + (s[s.len() - 1] == 'i') as usize)
+    );
 }
