95beb17d0e0b04c190de64c6ace864fbff6853ca
takagi3 <satoshi.takagi@pm.me>
2025-09-30 00:23:57 +0900

    2025/09/30

--- abc398/src/bin/b.rs
+++ abc398/src/bin/b.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: [usize; 7],
+    }
+
+    let mut cards: Vec<u32> = vec![0; 14];
+    a.iter().for_each(|&x| cards[x] += 1);
+
+    println!(
+        "{}",
+        if cards.iter().filter(|&x| *x >= 3).count() >= 1
+            && cards.iter().filter(|&x| *x >= 2).count() >= 2
+        {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
