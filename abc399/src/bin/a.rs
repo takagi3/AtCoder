e5cd2ec92605e1849f78ac2836d8b07504d74439
takagi3 <satoshi.takagi@pm.me>
2025-10-03 00:04:57 +0900

    2025/10/03

--- abc399/src/bin/a.rs
+++ abc399/src/bin/a.rs
@@ -1,3 +1,16 @@
+use proconio::{input, marker::Chars};
+
 fn main() {
-    todo!();
+    input! {
+        _n: usize,
+        s: Chars,
+        t: Chars,
+    }
+
+    let ans = s.iter()
+        .zip(t.iter())
+        .filter(|(a, b)| a != b)
+        .count();
+
+    println!("{}", ans);
 }
