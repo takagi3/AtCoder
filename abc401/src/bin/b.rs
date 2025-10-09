99dad24602cac5478943497027fb3205339c97d9
takagi3 <satoshi.takagi@pm.me>
2025-10-10 00:17:18 +0900

    2025/10/10

--- abc401/src/bin/b.rs
+++ abc401/src/bin/b.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        s: [String; n],
+    }
+
+    let mut auth = false;
+    let mut ans = 0;
+    for cmd in s {
+        match cmd.as_str() {
+            "login" => auth = true,
+            "logout" => auth = false,
+            "private" if !auth => ans += 1,
+            _ => (),
+        }
+    }
+
+    println!("{}", ans);
 }
