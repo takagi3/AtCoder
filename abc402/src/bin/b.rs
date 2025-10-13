1365a9b355417a7641221365885af31d2b33a0ee
takagi3 <satoshi.takagi@pm.me>
2025-10-14 02:54:12 +0900

    2025/10/14

--- abc402/src/bin/b.rs
+++ abc402/src/bin/b.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+use std::collections::VecDeque;
+
 fn main() {
-    todo!();
+    input! {
+        q: usize
+    }
+
+    let mut que = VecDeque::with_capacity(q);
+    let mut ans = Vec::new();
+    for _ in 0..q {
+        input! { t: u8 }
+        if t == 1 {
+            input! { x: i64 }
+            que.push_back(x);
+        } else {
+            ans.push(que.pop_front().unwrap().to_string());
+        }
+    }
+
+    println!("{}", ans.join("\n"));
 }
