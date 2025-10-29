bfd153a24a8aac9d98b00deaa9419ba7f5dc1f5e
takagi3 <satoshi.takagi@pm.me>
2025-10-29 22:51:24 +0900

    2025/10/29

--- abc405/src/bin/a.rs
+++ abc405/src/bin/a.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        r: u32,
+        x: u32,
+    }
+
+    println!(
+        "{}",
+        if x == 1 && 1600 <= r && r < 3000 || x == 2 && 1200 <= r && r < 2400 {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
