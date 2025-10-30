cb5d8d53e38d091e1070244b742143e0453722b0
takagi3 <satoshi.takagi@pm.me>
2025-10-31 03:10:44 +0900

    2025/10/31

--- abc405/src/bin/b.rs
+++ abc405/src/bin/b.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        a: [usize; n],
+    }
+
+    for i in 0..=n {
+        let mut kinds: Vec<bool> = vec![false; m + 1];
+        kinds[0] = true;
+        for j in 0..(n - i) {
+            kinds[a[j]] = true;
+        }
+        if !kinds.iter().all(|&x| x) {
+            println!("{}", i);
+            break;
+        }
+    }
 }
