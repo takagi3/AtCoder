6a3f61e2123a30e7eb4c27e36a51464bebd72bd4
takagi3 <satoshi.takagi@pm.me>
2025-11-06 02:49:16 +0900

    2025/11/06

--- abc406/src/bin/a.rs
+++ abc406/src/bin/a.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: u32,
+        b: u32,
+        c: u32,
+        d: u32,
+    }
+
+    println!(
+        "{}",
+        if a > c || a == c && b > d {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
